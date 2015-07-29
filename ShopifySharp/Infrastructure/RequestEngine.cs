ad9f06e3f23eed5721c8ad61e069c87dead9723f
Joshua Harms <joshua@nozzlegear.com>
2015-07-29 09:31:22 -0500

    Update exception handling.

--- ShopifySharp/Infrastructure/RequestEngine.cs
+++ ShopifySharp/Infrastructure/RequestEngine.cs
@@ -12,7 +12,7 @@ namespace ShopifySharp
 {
     internal static class RequestEngine
     {
-        public static async Task<T> ExecuteRequestAsync<T>(RestClient client, RestRequest request)
+        public static async Task<T> ExecuteRequestAsync<T>(RestClient client, RestRequest request) where T : new()
         {
             //Make request
             IRestResponse<T> response = await client.ExecuteTaskAsync<T>(request);
@@ -25,7 +25,9 @@ namespace ShopifySharp
             {
                 HttpStatusCode code = response.StatusCode;
                 ShopifyError error = JsonConvert.DeserializeObject<ShopifyError>(Encoding.UTF8.GetString(response.RawBytes));
-                string message = "Response status did not indicate success. Status: {0} - {1}.".FormatWith(response.StatusCode, response.StatusDescription);
+                string message = string.IsNullOrEmpty(error.Errors) ? 
+                    "Response did not indicate success. Status: {0} {1}.".FormatWith((int)code, response.StatusDescription) :
+                    error.Errors;
 
                 throw new ShopifyException(code, error, message);
             }
--- ShopifySharp/Infrastructure/ShopifyError.cs
+++ ShopifySharp/Infrastructure/ShopifyError.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace ShopifySharp
 {
     public class ShopifyError
     {
-
+        [JsonProperty("errors")]
+        public string Errors { get; set; }
     }
 }
