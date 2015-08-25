203c76a256988e3982b5416eea71d4d9cd42dbb2
Joshua Harms <joshua@nozzlegear.com>
2015-08-25 09:02:57 -0500

    Bugfix: ShopifyDiscountCode is nullable, but null value was not handled.

--- ShopifySharp/Enums/ShopifyDiscountCodeType.cs
+++ ShopifySharp/Enums/ShopifyDiscountCodeType.cs
@@ -6,22 +6,38 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System.Runtime.Serialization;
+using ShopifySharp.Converters;
 
 namespace ShopifySharp.Enums
 {
     /// <summary>
     /// The type of discount.
     /// </summary>
-    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(ShopifyDiscountCodeConverter))]
     public enum ShopifyDiscountCodeType
     {
+        /// <summary>
+        /// Discount is a percentage.
+        /// </summary>
         [EnumMember(Value = "percentage")]
         Percentage,
 
+        /// <summary>
+        /// Discount is for shipping.
+        /// </summary>
         [EnumMember(Value = "shipping")]
         Shipping,
 
+        /// <summary>
+        /// Discount is a fixed amount.
+        /// </summary>
         [EnumMember(Value = "fixed_amount")]
-        FixedAmount
+        FixedAmount,
+
+        /// <summary>
+        /// No discount.
+        /// </summary>
+        [EnumMember(Value = "none")]
+        None,
     }
 }
