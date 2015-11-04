6c966ae385d8c3c4ca937e92c274cf8320deca5d
Yitzchok Gottlieb <adminjew@gmail.com>
2015-11-04 13:42:37 -0500

    Adding Product Tests

---
+++ ShopifySharp.Tests/ShopifyProductService Tests/When_creating_a_product.cs
@@ -0,0 +1,44 @@
+﻿using Machine.Specifications;
+using ShopifySharp.Tests.Test_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace ShopifySharp.Tests
+{
+    [Subject(typeof(ShopifyProductService))]
+    public class When_creating_a_product
+    {
+        Establish context = () =>
+        {
+            Service = new ShopifyProductService(Utils.MyShopifyUrl, Utils.AccessToken);
+        };
+
+        Because of = () =>
+        {
+            Product = Service.CreateAsync(ProductCreation.CreateValidProducts()).Await().AsTask.Result;
+        };
+
+        It should_create_an_order = () =>
+        {
+            Product.ShouldNotBeNull();
+            Product.Id.HasValue.ShouldBeTrue();
+            Product.Title.ShouldNotBeNull();
+        };
+
+        Cleanup after = () =>
+        {
+            if (Product != null)
+            {
+                Service.DeleteAsync(Product.Id.Value).Await();
+            }
+        };
+
+        static ShopifyProductService Service;
+
+        static ShopifyProduct Product;
+    }
+}
