96f59b92e73b059a21f6e0d0f0a4a2111eebb6b4
Yitzchok Gottlieb <adminjew@gmail.com>
2015-11-04 14:46:49 -0500

    Add When_counting_products test

---
+++ ShopifySharp.Tests/ShopifyProductService Tests/When_counting_products.cs
@@ -0,0 +1,41 @@
+﻿using Machine.Specifications;
+using ShopifySharp.Tests.Test_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace ShopifySharp.Tests.Product
+{
+    [Subject(typeof(ShopifyProductService))]
+    public class When_counting_products
+    {
+        Establish context = () =>
+        {
+            Service = new ShopifyProductService(Utils.MyShopifyUrl, Utils.AccessToken);
+            Product = Service.CreateAsync(ProductCreation.CreateValidProducts(), new ShopifyProductCreateOptions() { Published = false }).Await().AsTask.Result;
+        };
+
+        Because of = () =>
+        {
+            Result = Service.CountAsync().Await().AsTask.Result;
+        };
+
+        It should_be_greater_than_zero = () => Result.ShouldBeGreaterThan(0);
+
+        Cleanup after = () =>
+        {
+            if (Product != null)
+            {
+                Service.DeleteAsync(Product.Id.Value).Await();
+            }
+        };
+
+        static ShopifyProductService Service;
+        static int Result = 0;
+
+        static ShopifyProduct Product;
+    }
+}
