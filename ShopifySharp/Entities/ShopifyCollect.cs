4a7d7c95f5d07dae3b4126793a801db4904a1b87
Stepan Kobzey <skobzey@channeliq.com>
2015-12-20 17:56:58 -0600

    Chnage Filter and Collect model to Pascla Casing

--- ShopifySharp/Entities/ShopifyCollect.cs
+++ ShopifySharp/Entities/ShopifyCollect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,36 +12,43 @@ namespace ShopifySharp
         /// <summary>
         /// The id of the custom collection containing the product.
         /// </summary>
-        public long collection_id { get; set; }
+        [JsonProperty("collection_id")]
+        public long CollectionId { get; set; }
 
         /// <summary>
         /// The unique numeric identifier for the product in the custom collection.
         /// </summary>
-        public long product_id { get; set; }
+        [JsonProperty("product_id")]
+        public long ProductId { get; set; }
 
         /// <summary>
         /// States whether or not the collect is featured.
         /// </summary>
-        public bool featured { get; set; }
+        [JsonProperty("featured")]
+        public bool Featured { get; set; }
 
         /// <summary>
         /// The date and time when the collect was created. The API returns this value in ISO 8601 format.
         /// </summary>
-        public DateTime created_at { get; set; }
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
 
         /// <summary>
         /// The date and time when the collect was last updated. The API returns this value in ISO 8601 format.
         /// </summary>
-        public DateTime updated_at { get; set; }
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
 
         /// <summary>
         /// A number specifying the manually sorted position of this product in a custom collection. The first position is 1. This value only applies when the custom collection is viewed using the Manual sort order.
         /// </summary>
-        public int position { get; set; }
+        [JsonProperty("position")]
+        public int Position { get; set; }
 
         /// <summary>
         /// This is the same value as position but padded with leading zeroes to make it alphanumeric-sortable.
         /// </summary>
-        public string sort_value { get; set; }
+        [JsonProperty("sort_value")]
+        public string SortValue { get; set; }
     }
 }
--- ShopifySharp/Services/Collect/ShopifyCollectFilterOptions.cs
+++ ShopifySharp/Services/Collect/ShopifyCollectFilterOptions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,17 @@ namespace ShopifySharp
 {
     public class ShopifyCollectFilterOptions : ShopifyFilterOptions
     {
+        /// <summary>
+        /// An optional product id to retrieve. 
+        /// </summary>
+        [JsonProperty("product_id")]
+        public long? ProductId { get; set; }
 
-        public long? product_id { get; set; }
-        public long? collection_id { get; set; }
+        /// <summary>
+        /// An optional collection id to retrieve. 
+        /// </summary>
+        [JsonProperty("collection_id")]
+        public long? CollectionId { get; set; }
 
     }
 }
