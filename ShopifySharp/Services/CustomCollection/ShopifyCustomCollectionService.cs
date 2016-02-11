f04a01d82d0d28b37934816984fb6e9544fd6716
ishahrier <developer@exico.ca>
2016-02-10 23:18:42 -0600

    Get Methods for Smart and Custom Collection
    No testing is done for them. But I am using them for my projects already
    and they work fine. Also the create and delete methods will be written
    when I will get time or somebody else might want to write them.

---
+++ ShopifySharp/Services/CustomCollection/ShopifyCustomCollectionService.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopifySharp;
+namespace ShopifySharp
+{
+    /// <summary>
+    /// A service for getting data and setting data about Shopify Custom collection objects
+    /// </summary>
+    public class ShopifyCustomCollectionService : ShopifyService
+    {
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="myShopifyUrl">The shop's *.myshopify.com URL.</param>
+        /// <param name="shopAccessToken">An API access token for the shop.</param>
+        public ShopifyCustomCollectionService(string myShopifyUrl, string shopAccessToken) : base(myShopifyUrl, shopAccessToken) { }
+
+        #endregion Constructor
+
+        #region Public, non-static methods
+
+        /// <summary>
+        /// Get a count of all Custom collections that contain a given product
+        /// </summary>
+        /// <returns>The count of all Custom collections for the shop.</returns>
+        public async Task<int> CountAsync(ShopifyCustomCollectionFilterOptions options = null)
+        {
+            IRestRequest req = RequestEngine.CreateRequest("custom_collections/count.json", Method.GET, "count");
+
+            if (options != null) req.Parameters.AddRange(options.ToParameters(ParameterType.GetOrPost));
+
+            JToken responseObject = await RequestEngine.ExecuteRequestAsync(_RestClient, req);
+
+            return responseObject.Value<int>("count");
+        }
+
+        /// <summary>
+        /// Get a list of all Custom collections 
+        /// </summary>
+        /// <returns>The <see cref="ShopifyCustomCollection"/></returns>
+        public async Task<IEnumerable<ShopifyCustomCollection>> ListAsync(ShopifyCustomCollectionFilterOptions options = null)
+        {
+            IRestRequest req = RequestEngine.CreateRequest("custom_collections.json", Method.GET, "custom_collections");
+
+            if (options != null) req.Parameters.AddRange(options.ToParameters(ParameterType.GetOrPost));
+
+            return await RequestEngine.ExecuteRequestAsync<List<ShopifyCustomCollection>>(_RestClient, req);
+        }
+
+        /// <summary>
+        /// Retrieves the <see cref="ShopifyCustomCollection"/> with the given id.
+        /// </summary>
+        /// <param name="CustomCollecttionId">The id of the collect to retrieve.</param>
+        /// <param name="fields">A comma-separated list of fields to return.</param>
+        /// <returns>The <see cref="ShopifyCustomCollection"/>.</returns>
+        public async Task<ShopifyCustomCollection> GetAsync(long CustomCollecttionId, string fields = null)
+        {
+            IRestRequest req = RequestEngine.CreateRequest($"custom_collections/{CustomCollecttionId}.json", Method.GET, "custom_collection");
+            if (string.IsNullOrEmpty(fields) == false)
+            {
+                req.AddParameter("fields", fields);
+            }
+            return await RequestEngine.ExecuteRequestAsync<ShopifyCustomCollection>(_RestClient, req);
+        }
+
+        #endregion Public, non-static methods
+    }
+}
---
+++ ShopifySharp/Services/SmartCollection/ShopifySmartCollectionService.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopifySharp
+{
+    /// <summary>
+    /// A service for getting data and setting data about Shopify smart collection objects
+    /// </summary>
+    public class ShopifySmartCollectionService : ShopifyService
+    {
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="myShopifyUrl">The shop's *.myshopify.com URL.</param>
+        /// <param name="shopAccessToken">An API access token for the shop.</param>
+        public ShopifySmartCollectionService(string myShopifyUrl, string shopAccessToken) : base(myShopifyUrl, shopAccessToken) { }
+
+        #endregion Constructor
+
+        #region Public, non-static methods
+
+        /// <summary>
+        /// Get a count of all smart collections that contain a given product
+        /// </summary>
+        /// <returns>The count of all smart collections for the shop.</returns>
+        public async Task<int> CountAsync(ShopifySmartCollectionFilterOptions options = null)
+        {
+            IRestRequest req = RequestEngine.CreateRequest("smart_collections/count.json", Method.GET, "count");
+
+            if (options != null) req.Parameters.AddRange(options.ToParameters(ParameterType.GetOrPost));
+
+            JToken responseObject = await RequestEngine.ExecuteRequestAsync(_RestClient, req);
+
+            return responseObject.Value<int>("count");
+        }
+
+        /// <summary>
+        /// Get a list of all smart collections 
+        /// </summary>
+        /// <returns>The <see cref="ShopifySmartCollection"/></returns>
+        public async Task<IEnumerable<ShopifySmartCollection>> ListAsync(ShopifySmartCollectionFilterOptions options = null)
+        {
+            IRestRequest req = RequestEngine.CreateRequest("smart_collections.json", Method.GET, "smart_collections");
+
+            if (options != null) req.Parameters.AddRange(options.ToParameters(ParameterType.GetOrPost));
+
+            return await RequestEngine.ExecuteRequestAsync<List<ShopifySmartCollection>>(_RestClient, req);
+        }
+
+        /// <summary>
+        /// Retrieves the <see cref="ShopifySmartCollection"/> with the given id.
+        /// </summary>
+        /// <param name="smartCollecttionId">The id of the collect to retrieve.</param>
+        /// <param name="fields">A comma-separated list of fields to return.</param>
+        /// <returns>The <see cref="ShopifySmartCollection"/>.</returns>
+        public async Task<ShopifySmartCollection> GetAsync(long smartCollecttionId, string fields = null)
+        {
+            IRestRequest req = RequestEngine.CreateRequest($"smart_collections/{smartCollecttionId}.json", Method.GET, "smart_collection");
+            if (string.IsNullOrEmpty(fields) == false)
+            {
+                req.AddParameter("fields", fields);
+            }
+            return await RequestEngine.ExecuteRequestAsync<ShopifySmartCollection>(_RestClient, req);
+        }
+
+        #endregion Public, non-static methods
+    }
+}
