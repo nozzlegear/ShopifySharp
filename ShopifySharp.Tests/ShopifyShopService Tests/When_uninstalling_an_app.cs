b769bc61a15d5f14a195a1d7b550097c916ba295
Joshua Harms <joshua@nozzlegear.com>
2016-04-12 14:46:15 -0500

    ShopifyShopService.UninstallAppAsync

---
+++ ShopifySharp.Tests/ShopifyShopService Tests/When_uninstalling_an_app.cs
@@ -0,0 +1,33 @@
+﻿using Machine.Specifications;
+using System;
+
+namespace ShopifySharp.Tests
+{
+    [Subject(typeof(ShopifyShopService))]
+    public class When_uninstalling_an_app
+    {
+        Establish context = () =>
+        {
+            _Service = new ShopifyShopService(Utils.MyShopifyUrl, Utils.AccessToken);
+        };
+
+        Because of = () =>
+        {
+            _Service.GetAsync().Await();
+        };
+
+        [Ignore("This test cannot be completed because private apps (the kind used to test this library) cannot be uninstalled.")]
+        It should_uninstall_the_app = () =>
+        {
+            _Shop.ShouldNotBeNull();
+        };
+
+        Cleanup after = () =>
+        {
+
+        };
+
+        static ShopifyShopService _Service;
+        static ShopifyShop _Shop;
+    }
+}
--- ShopifySharp/Services/Shop/ShopifyShopService.cs
+++ ShopifySharp/Services/Shop/ShopifyShopService.cs
@@ -34,6 +34,16 @@ namespace ShopifySharp
             return await RequestEngine.ExecuteRequestAsync<ShopifyShop>(_RestClient, request);
         }
 
+        /// <summary>
+        /// Forces the shop to uninstall your Shopify app. Uninstalling an application is an irreversible operation. Be entirely sure that you no longer need to make API calls for the shop in which the application has been installed.
+        /// </summary>
+        public async Task UninstallAppAsync()
+        {
+            var request = RequestEngine.CreateRequest("shop.json", Method.DELETE);
+
+            await RequestEngine.ExecuteRequestAsync(_RestClient, request);
+        }
+
         #endregion
     }
 }
