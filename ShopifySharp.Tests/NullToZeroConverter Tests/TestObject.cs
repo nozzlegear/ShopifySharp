2548d71bb961a7d18e43d38bc42a004e7adcdd7e
Joshua Harms <joshua@nozzlegear.com>
2016-04-17 11:55:47 -0500

    Added test for NullToZeroConverter

---
+++ ShopifySharp.Tests/NullToZeroConverter Tests/TestObject.cs
@@ -0,0 +1,7 @@
+﻿namespace ShopifySharp.Tests.NullToZeroConverter_Tests
+{
+    class TestObject
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ ShopifySharp.Tests/NullToZeroConverter Tests/When_deserializing_null_int.cs
@@ -0,0 +1,50 @@
+﻿using Machine.Specifications;
+using Newtonsoft.Json;
+using ShopifySharp.Converters;
+using System;
+
+namespace ShopifySharp.Tests.NullToZeroConverter_Tests
+{
+    [Subject(typeof(NullToZeroConverter))]
+    class When_deserializing_null_int
+    {
+        Establish context = () =>
+        {
+
+        };
+
+        Because of = () =>
+        {
+            //Normal deserialization should fail to convert null or string to int
+            Ex = Catch.Exception(() => JsonConvert.DeserializeObject<TestObject>(EmptyValueString));
+
+            //Deserializing with NullToZeroConverter should not throw exception
+            EmptyValueResult = JsonConvert.DeserializeObject<TestObject>(EmptyValueString, new NullToZeroConverter());
+
+            //Normal deserialization
+            ValueResult = JsonConvert.DeserializeObject<TestObject>(ValueString, new NullToZeroConverter());
+        };
+
+        It should_deserialize_null_to_zero = () =>
+        {
+            EmptyValueResult.Value.ShouldEqual(0);
+            ValueResult.Value.ShouldEqual(5);
+            Ex.ShouldNotBeNull();
+        };
+
+        Cleanup after = () =>
+        {
+
+        };
+
+        static TestObject EmptyValueResult;
+
+        static TestObject ValueResult;
+
+        static Exception Ex;
+        
+        static string EmptyValueString = "{ \"Value\" : \"\" }";
+
+        static string ValueString = " { \"Value\" : 5 } ";
+    }
+}
