34deff6e6e58b1d5d45b90382dadc48784cf3fca
Tommy Holm Jakobsen <tommy@holmjakobsen.dk>
2016-11-14 14:38:25 +0100

    Support for cancelling orders

---
+++ ShopifySharp.Tests/ShopifyOrderService Tests/When_cancelling_an_order.cs
@@ -0,0 +1,39 @@
+﻿using Machine.Specifications;
+using ShopifySharp.Tests.Test_Data;
+
+namespace ShopifySharp.Tests
+{
+    [Subject(typeof(ShopifyOrderService))]
+    public class When_cancelling_an_order
+    {
+        Establish context = () =>
+        {
+            Service = new ShopifyOrderService(Utils.MyShopifyUrl, Utils.AccessToken);
+            Id = Service.CreateAsync(OrderCreation.GenerateOrder()).Await().AsTask.Result.Id.Value;
+        };
+
+        private Because of = () =>
+        {
+            Service.CancelAsync(Id).Await();
+            Order = Service.GetAsync(Id).Await().AsTask.Result;
+        };
+
+        It should_cancel_an_order = () =>
+        {
+            Order.ShouldNotBeNull();
+            Order.Id.ShouldEqual(Id);
+            Order.CancelledAt.HasValue.ShouldBeTrue();
+        };
+
+        Cleanup after = () =>
+        {
+            Service.DeleteAsync(Id).Await();
+        };
+
+        static ShopifyOrderService Service;
+
+        static ShopifyOrder Order;
+
+        static long Id;
+    }
+}
