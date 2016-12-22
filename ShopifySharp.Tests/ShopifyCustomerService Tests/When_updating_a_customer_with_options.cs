cc6405bcd8c441864ee4f0014de49c97979b9c0e
Ernesto <smernesto@hotmail.com>
2016-12-21 23:17:47 -0500

    Add support for changing the customer's password

---
+++ ShopifySharp.Tests/ShopifyCustomerService Tests/When_updating_a_customer_with_options.cs
@@ -0,0 +1,47 @@
+﻿using Machine.Specifications;
+using ShopifySharp.Tests.Test_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifySharp.Tests
+{
+    [Subject(typeof(ShopifyCustomerService))]
+    public class When_updating_a_customer_with_options
+    {
+        Establish context = () =>
+        {
+            _Service = new ShopifyCustomerService(Utils.MyShopifyUrl, Utils.AccessToken);
+
+            //Create a customer to update
+            _Customer = _Service.CreateAsync(CustomerCreation.CreateValidCustomer()).Await().AsTask.Result;
+
+            //Change the customer's email address
+            _Customer.Email = "test-update@example.com";
+        };
+
+        Because of = () =>
+        {
+            _Customer = _Service.UpdateAsync(_Customer, new ShopifyCustomerUpdateOptions()
+            {
+                Password = "loktarogar",
+                PasswordConfirmation = "loktarogar"
+            }).Await().AsTask.Result;
+        };
+
+        It should_update_the_customer_with_options = () =>
+        {
+            _Customer.Email.Equals("test-update@example.com").ShouldBeTrue();
+        };
+
+        Cleanup after = () =>
+        {
+            _Service.DeleteAsync(_Customer.Id.Value).Await();
+        };
+
+        static ShopifyCustomerService _Service;
+        static ShopifyCustomer _Customer;
+    }
+}
---
+++ ShopifySharp/Services/Customer/ShopifyCustomerUpdateOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ShopifySharp
+{
+    public class ShopifyCustomerUpdateOptions
+    {
+        /// <summary>
+        /// An optional password for the user. Default is null.
+        /// </summary>
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Should be set and match <see cref="Password"/>. Default is null.
+        /// </summary>
+        [JsonProperty("password_confirmation")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
