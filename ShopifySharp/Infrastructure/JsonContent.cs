dd3894d499112c3ffab736259a5f9f3d638f7191
Joshua Harms <joshua@nozzlegear.com>
2017-03-30 16:33:00 -0500

    Added JsonContent class, to be used when posting/putting JSON.

---
+++ ShopifySharp/Infrastructure/JsonContent.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ShopifySharp.Infrastructure
+{
+    public class JsonContent : ByteArrayContent
+    {
+        public JsonContent(object data) : base(ToBytes(data))
+        {
+            Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+
+        private static byte[] ToBytes(object data)
+        {
+            var rawData = JsonConvert.SerializeObject(data);
+
+            return Encoding.UTF8.GetBytes(rawData);
+        }
+    }
+}
