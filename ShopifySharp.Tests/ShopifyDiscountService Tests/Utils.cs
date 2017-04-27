9f50a021716521f797fd8c8de908e002eea540a2
Joshua Harms <joshua@nozzlegear.com>
2017-04-26 20:21:02 -0500

    Converted discounts to 4.0.

--- ShopifySharp.Tests/ShopifyDiscountService Tests/Utils.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShopifySharp.Tests.ShopifyDiscountService_Tests
-{
-    static class DiscountUtils
-    {
-        public static ShopifyDiscountService Service = new ShopifyDiscountService(Utils.MyShopifyUrl, Utils.AccessToken);
-    }
-}
