29241e4d043d5baa716369871dcc85233977b3eb
Victor Leontyev <victor.leontev@gmail.com>
2017-08-27 22:16:16 +0400

    policies API

---
+++ ShopifySharp/Entities/Policy.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using System;
+
+namespace ShopifySharp
+{
+    public class Policy
+    {
+        /// <summary>
+        /// The name of the policy.
+        /// </summary>
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The content of the policy.
+        /// </summary>
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        /// <summary>
+        /// The public URL to policy.
+        /// </summary>
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// The date and time when the policy was created. 
+        /// </summary>
+        [JsonProperty("created_at")]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        /// <summary>
+        /// The date and time when the policy was last modified. 
+        /// </summary>
+        [JsonProperty("updated_at")]
+        public DateTimeOffset? UpdatedAt { get; set; }
+    }
+}
