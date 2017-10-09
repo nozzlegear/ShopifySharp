1960d0e82f6de38b652080b49349d51eece0a614
lasamuadib <jonathan.nordell@gmail.com>
2017-10-09 13:00:16 -0500

    Added Abandoned checkout functionality

---
+++ ShopifySharp.Tests/Checkout_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopifySharp.Tests
+{
+    [Trait("Category", "Checkout")]
+    public class Checkout_Tests
+    {
+
+        private CheckoutService _Service { get; } = new CheckoutService(Utils.MyShopifyUrl, Utils.AccessToken);
+
+        [Fact]
+        public async Task Counts_Checkouts()
+        {
+            var count = await _Service.CountAsync();
+
+            Assert.True(count > 0);
+        }
+
+        [Fact]
+        public async Task Lists_Checkouts()
+        {
+            var list = await _Service.ListAsync();
+
+            Assert.True(list.Count() > 0);
+        }
+    }
+}
---
+++ ShopifySharp/Services/Checkout/CheckoutService.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using ShopifySharp.Filters;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ShopifySharp.Infrastructure;
+
+namespace ShopifySharp
+{
+    /// <summary>
+    /// A service for manipulating Shopify orders.
+    /// </summary>
+    public class CheckoutService : ShopifyService
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="OrderService" />.
+        /// </summary>
+        /// <param name="myShopifyUrl">The shop's *.myshopify.com URL.</param>
+        /// <param name="shopAccessToken">An API access token for the shop.</param>
+        public CheckoutService(string myShopifyUrl, string shopAccessToken) : base(myShopifyUrl, shopAccessToken) { }
+
+        /// <summary>
+        /// Gets a count of all of the shop's abandoned checkouts.
+        /// </summary>
+        /// <param name="filter">Options for filtering the count.</param>
+        /// <returns>The count of all orders for the shop.</returns>
+        public virtual async Task<int> CountAsync(CheckoutFilter filter = null)
+        {
+            var req = PrepareRequest("checkouts/count.json");
+
+            if (filter != null)
+            {
+                req.Url.QueryParams.AddRange(filter.ToParameters());
+            }
+
+            return await ExecuteRequestAsync<int>(req, HttpMethod.Get, rootElement: "count");
+        }
+
+        /// <summary>
+        /// Gets a list of up to 250 of the shop's abandoned carts.
+        /// </summary>
+        /// <returns></returns>
+        public virtual async Task<IEnumerable<Checkout>> ListAsync(CheckoutFilter options = null)
+        {
+            var req = PrepareRequest("checkouts.json");
+
+            if (options != null)
+            {
+                req.Url.QueryParams.AddRange(options.ToParameters());
+            }
+
+            return await ExecuteRequestAsync<List<Checkout>>(req, HttpMethod.Get, rootElement: "checkouts");
+        }
+
+    }
+}
