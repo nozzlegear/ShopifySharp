82b2582026037f0bcafa6218ab70fc6de1f7ce1b
Clement <clement.gutel@gmail.com>
2017-10-30 21:03:57 +1100

    The SmartRetryExecutionPolicy will now dynamically resize the leacky bucket size (useful for Shopify Plus customers, who have a bigger bucket)

--- ShopifySharp/Infrastructure/Policies/SmartRetryExecutionPolicy.LeakyBucket.cs
+++ ShopifySharp/Infrastructure/Policies/SmartRetryExecutionPolicy.LeakyBucket.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,13 +9,17 @@ namespace ShopifySharp
     {
         private class LeakyBucket
         {
-            private const int BUCKET_MAX_SIZE = 40;
+            private const int DEFAULT_BUCKET_CAPACITY = 40;
+
+            private int _bucketCapacity = DEFAULT_BUCKET_CAPACITY;
+
+            private int _leakRate = 1;
 
             private static ConcurrentBag<LeakyBucket> _allLeakyBuckets = new ConcurrentBag<LeakyBucket>();
             
             private static Timer _dripAllBucketsTimer = new Timer(_ => DripAllBuckets(), null, THROTTLE_DELAY, THROTTLE_DELAY);
 
-            private SemaphoreSlim _semaphore = new SemaphoreSlim(BUCKET_MAX_SIZE, BUCKET_MAX_SIZE);
+            private SemaphoreSlim _semaphore = new SemaphoreSlim(DEFAULT_BUCKET_CAPACITY, DEFAULT_BUCKET_CAPACITY);
 
             public LeakyBucket()
             {
@@ -26,13 +31,36 @@ namespace ShopifySharp
                 return _semaphore.WaitAsync();
             }
 
-            public void SetContentSize(int contentSize)
+            public void SetState(LeakyBucketState bucketInfo)
             {
+                //Shopify Plus customers have a bucket that is twice the size (80) so we resize the bucket capacity accordingly
+                //It is apparently possible to request the bucket size to be even larger
+                //https://ecommerce.shopify.com/c/shopify-apis-and-technology/t/what-is-the-default-api-call-limit-on-shopify-stores-407292
+                //Note that when the capacity doubles, the leak rate also doubles. So, not only can request bursts be larger, it is also possible to sustain a faster rate over the long term.
+                if (bucketInfo.Capacity != this._bucketCapacity)
+                {
+                    lock (_semaphore)
+                    {
+                        if (bucketInfo.Capacity != this._bucketCapacity)
+                        {
+                            int waitingOperations = _bucketCapacity - _semaphore.CurrentCount;
+                            var oldSemaphore = Interlocked.Exchange(ref _semaphore, new SemaphoreSlim(bucketInfo.Capacity - waitingOperations, bucketInfo.Capacity));
+                            //release all operations waiting on the old semaphore
+                            if (waitingOperations > 0)
+                            {
+                                oldSemaphore.Release(waitingOperations);
+                            }
+                            oldSemaphore.Dispose();
+                            _leakRate = bucketInfo.Capacity / DEFAULT_BUCKET_CAPACITY;
+                            _bucketCapacity = bucketInfo.Capacity;
+                        }
+                    }
+                }
                 //Corrects the grant capacity of the bucket based on the size returned by Shopify.
                 //Shopify may know that the remaining capacity is less than we think it is (for example if multiple programs are using that same token)
                 //Shopify may also think that the remaining capacity is more than we know, but we do not ever empty the bucket because Shopify is not
                 //considering requests that we know are already in flight.
-                int grantCapacity = BUCKET_MAX_SIZE - contentSize;
+                int grantCapacity = this._bucketCapacity - bucketInfo.CurrentFillLevel;
 
                 while (_semaphore.CurrentCount > grantCapacity)
                 {
@@ -41,11 +69,12 @@ namespace ShopifySharp
                 }
             }
 
-            private void ReleaseOne()
+            private void Drip()
             {
-                if (_semaphore.CurrentCount < BUCKET_MAX_SIZE)
+                if (_semaphore.CurrentCount < _bucketCapacity)
                 {
-                    _semaphore.Release();
+                    int waitingOperations = _bucketCapacity - _semaphore.CurrentCount;
+                    _semaphore.Release(Math.Min(_leakRate, waitingOperations));
                 }
             }
 
@@ -53,7 +82,7 @@ namespace ShopifySharp
             {
                 foreach (var bucket in _allLeakyBuckets)
                 {
-                    bucket.ReleaseOne();
+                    bucket.Drip();
                 }
             }
         }
