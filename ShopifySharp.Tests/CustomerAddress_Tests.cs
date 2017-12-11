f06622a95af1a9955d3b051a2812b2eec48e2ba9
Joshua Harms <joshua@nozzlegear.com>
2017-12-11 10:59:15 -0600

    Fix tests

--- ShopifySharp.Tests/CustomerAddress_Tests.cs
+++ ShopifySharp.Tests/CustomerAddress_Tests.cs
@@ -29,12 +29,12 @@ namespace ShopifySharp.Tests
         [Fact]
         public async Task Deletes_Addresses()
         {
-            //var Address = await Fixture.Create(true);
+            var created = await Fixture.Create("123 4th St", true);
             bool threw = false;
 
             try
             {
-                await Fixture.Service.DeleteAsync(Fixture.CustomerId.Value, Fixture.Created.First().Id.Value);
+                await Fixture.Service.DeleteAsync(Fixture.CustomerId.Value, created.Id.Value);
             }
             catch (ShopifyException ex)
             {
@@ -49,7 +49,8 @@ namespace ShopifySharp.Tests
         [Fact]
         public async Task Gets_Addresses()
         {
-            var address = await Fixture.Service.GetAsync(Fixture.CustomerId.Value, Fixture.Created.First().Id.Value);
+            var created = await Fixture.Create("234 5th St");
+            var address = await Fixture.Service.GetAsync(Fixture.CustomerId.Value, created.Id.Value);
 
             Assert.NotNull(address);
             Assert.Equal(Fixture.FirstName, address.FirstName);
@@ -59,22 +60,27 @@ namespace ShopifySharp.Tests
         [Fact]
         public async Task Creates_Addresses()
         {
-            var address = await Fixture.Create();
+            var address = await Fixture.Create("345 6th St");
 
             Assert.NotNull(address);
             Assert.Equal(Fixture.FirstName, address.FirstName);
             Assert.Equal(Fixture.LastName, address.LastName);
+
+            await Fixture.Service.DeleteAsync(Fixture.CustomerId.Value, address.Id.Value);
         }
 
-
-
+        [Fact]
         public async Task Updates_Addresss()
         {
             string firstName = "Jane";
-            var created = await Fixture.Create();
+            string lastName = "Doe";
+            string fullName = "Jane Doe";
+            var created = await Fixture.Create("456 7th St");
             long id = created.Id.Value;
 
             created.FirstName = firstName;
+            created.LastName = lastName;
+            created.Name = fullName;
             created.Id = null;
 
             var updated = await Fixture.Service.UpdateAsync(Fixture.CustomerId.Value, id, created);
@@ -89,10 +95,10 @@ namespace ShopifySharp.Tests
     public class CustomerAddress_Tests_Fixture : IAsyncLifetime
     {
         public CustomerAddressService Service { get; } = new CustomerAddressService(Utils.MyShopifyUrl, Utils.AccessToken);
+
         public string FirstName => "John";
 
         public string LastName => "Doe";
-
 
         public long? CustomerId { get; set; }
 
@@ -128,20 +134,20 @@ namespace ShopifySharp.Tests
         }
 
         /// <summary>
-        /// Convenience function for running tests. Creates the object and automatically adds it to the queue for deleting after tests finish.
+        /// Convenience function for running tests. Gets an object from the list of already created objects, or creates the object and automatically adds it to the queue for deleting after tests finish.
         /// </summary>
-        public async Task<Address> Create(bool skipAddToDeleteList = false)
+        public async Task<Address> Create(string streetAddress, bool skipAddToDeleteList = false)
         {
             var obj = await Service.CreateAsync(CustomerId.Value, new Address()
             {
-                Address1 = "123 4th Street",
+                Address1 = streetAddress,
                 City = "Minneapolis",
                 Province = "Minnesota",
                 ProvinceCode = "MN",
                 Zip = "55401",
                 Phone = "555-555-5555",
-                FirstName = "John",
-                LastName = "Doe",
+                FirstName = FirstName,
+                LastName = LastName,
                 Company = "Tomorrow Corporation",
                 Country = "United States",
                 CountryCode = "US",
