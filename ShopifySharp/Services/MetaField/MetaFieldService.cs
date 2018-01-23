0381526027f3e329dd8af817a75eefe639e2ecf5
Victor Leontyev <victor@prototype.ae>
2018-01-23 11:15:12 +0400

    metafields service update. Support product variants and blog articles

--- ShopifySharp/Services/MetaField/MetaFieldService.cs
+++ ShopifySharp/Services/MetaField/MetaFieldService.cs
@@ -51,6 +51,19 @@ namespace ShopifySharp
             return await _CountAsync($"{resourceType}/{resourceId}/metafields/count.json", filter);
         }
 
+        /// <summary>
+        /// Gets a count of the metafields for the given entity type and filter options.
+        /// </summary>
+        /// <param name="resourceType">The type of shopify resource to obtain metafields for. This could be variants, products, orders, customers, custom_collections.</param>
+        /// <param name="resourceId">The Id for the resource type.</param>
+        /// <param name="parentResourceType">The type of shopify parent resource to obtain metafields for. This could be blogs, products.</param>
+        /// <param name="parentResourceId">The Id for the resource type.</param>
+        /// <param name="filter">Options to filter the results.</param>
+        public virtual async Task<int> CountAsync(long resourceId, string resourceType, long parentResourceId, string parentResourceType,  MetaFieldFilter filter = null)
+        {
+            return await _CountAsync($"{parentResourceType}/{parentResourceId}/{resourceType}/{resourceId}/metafields/count.json", filter);
+        }
+
         private async Task<IEnumerable<MetaField>> _ListAsync(string path, MetaFieldFilter filter = null)
         {
             var req = PrepareRequest(path);
@@ -84,6 +97,20 @@ namespace ShopifySharp
         }
 
         /// <summary>
+        /// Gets a list of the metafields for the given entity type and filter options.
+        /// </summary>
+        /// <param name="parentResourceType">The type of shopify parentresource to obtain metafields for. This could be blogs, products.</param>
+        /// <param name="parentResourceId">The Id for the parent resource type.</param>
+        /// <param name="resourceType">The type of shopify resource to obtain metafields for. This could be variants, products, orders, customers, custom_collections.</param>
+        /// <param name="resourceId">The Id for the resource type.</param>
+        /// <param name="filter">Options to filter the results.</param>
+        public virtual async Task<IEnumerable<MetaField>> ListAsync(long resourceId, string resourceType, long parentResourceId, string parentResourceType, MetaFieldFilter filter = null)
+        {
+            return await _ListAsync($"{parentResourceType}/{parentResourceId}/{resourceType}/{resourceId}/metafields.json", filter);
+        }
+
+
+        /// <summary>
         /// Retrieves the metafield with the given id.
         /// </summary>
         /// <param name="metafieldId">The id of the metafield to retrieve.</param>
@@ -107,6 +134,25 @@ namespace ShopifySharp
         public virtual async Task<MetaField> CreateAsync(MetaField metafield)
         {
             var req = PrepareRequest("metafields.json");
+            var content = new JsonContent(new
+            {
+                metafield = metafield
+            });
+
+            return await ExecuteRequestAsync<MetaField>(req, HttpMethod.Post, content, "metafield");
+        }
+
+        /// <summary>
+        /// Creates a new metafield on the given entity.
+        /// </summary>
+        /// <param name="metafield">A new metafield. Id should be set to null.</param>
+        /// <param name="resourceId">The Id of the resource the metafield will be associated with. This can be variants, products, orders, customers, custom_collections, etc.</param>
+        /// <param name="resourceType">The resource type the metaifeld will be associated with. This can be variants, products, orders, customers, custom_collections, etc.</param>
+        /// <param name="parentResourceId">The Id of the parent resource the metafield will be associated with. This can be blogs, products.</param>
+        /// <param name="parentResourceType">The resource type the metaifeld will be associated with. This can be articles, variants.</param>
+        public virtual async Task<MetaField> CreateAsync(MetaField metafield, long resourceId, string resourceType, long parentResourceId, string parentResourceType)
+        {
+            var req = PrepareRequest($"{parentResourceType}/{parentResourceId}/{resourceType}/{resourceId}/metafields.json");
             var content = new JsonContent(new
             {
                 metafield = metafield
