611dcfdb3446a00c0270f041dcecd8a2370025c7
davron12 <davron12@hotmail.com>
2018-05-24 18:15:51 +0000

    Added descriptions to undocumented Fulfillment.Destination properties

--- ShopifySharp/Entities/Destination.cs
+++ ShopifySharp/Entities/Destination.cs
@@ -12,42 +12,49 @@ namespace ShopifySharp
     {
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to recipient's first name
         /// </summary>
         [JsonProperty("first_name")]
         public string FirstName { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to address line 1
         /// </summary>
         [JsonProperty("address1")]
         public string Address1 { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to phone number
         /// </summary>
         [JsonProperty("phone")]
         public string Phone { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to city
         /// </summary>
         [JsonProperty("city")]
         public string City { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Zip/postal code
         /// </summary>
         [JsonProperty("zip")]
         public string Zip { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Fullstate/province name
         /// </summary>
         [JsonProperty("province")]
         public string Province { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Full country name
         /// </summary>
         [JsonProperty("country")]
         public string Country { get; set; }
@@ -60,42 +67,49 @@ namespace ShopifySharp
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to address line 2
         /// </summary>
         [JsonProperty("address2")]
         public string Address2 { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to company name
         /// </summary>
         [JsonProperty("company")]
         public string Company { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to location's latitude
         /// </summary>
         [JsonProperty("latitude")]
         public decimal? Latitude { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to location's longitude
         /// </summary>
         [JsonProperty("longitude")]
         public decimal? Longitude { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Ship-to recipient's full name
         /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Two letter country code abbreviation
         /// </summary>
         [JsonProperty("country_code")]
         public string CountryCode { get; set; }
 
         /// <summary>
         /// This property is undocumented by Shopify.
+        /// Two letter state/province abbreviation
         /// </summary>
         [JsonProperty("province_code")]
         public string ProvinceCode { get; set; }
