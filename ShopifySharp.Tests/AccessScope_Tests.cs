58901ac9f79221b773dc75e47341f00d8ee29d4e
Clement <clement.gutel@gmail.com>
2018-06-28 12:40:03 +1000

    New AccessScopeService class to get access scopes https://help.shopify.com/api/reference/access/accessscope

---
+++ ShopifySharp.Tests/AccessScope_Tests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using EmptyAssert = ShopifySharp.Tests.Extensions.EmptyExtensions;
+
+namespace ShopifySharp.Tests
+{
+    [Trait("Category", "Access scope")]
+    public class AccessScope_Tests : IClassFixture<AccessScope_Tests_Fixture>
+    {
+        private AccessScope_Tests_Fixture Fixture { get; }
+
+        public AccessScope_Tests(AccessScope_Tests_Fixture fixture)
+        {
+            this.Fixture = fixture;
+        }
+
+        [Fact]
+        public async Task List()
+        {
+            var scopes = await Fixture.Service.ListAsync();
+            Assert.True(scopes.Count() > 0);
+        }
+    }
+
+    public class AccessScope_Tests_Fixture : IAsyncLifetime
+    {
+        public AccessScopeService Service { get; } = new AccessScopeService(Utils.MyShopifyUrl, Utils.AccessToken);
+
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ShopifySharp/Services/AccessScope/AccessScopeService.cs
@@ -0,0 +1,30 @@
+﻿using ShopifySharp.Filters;
+using ShopifySharp.Infrastructure;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ShopifySharp
+{
+    /// <summary>
+    /// A service for getting the access scopes associated with the access token
+    /// </summary>
+    public class AccessScopeService : ShopifyService
+    {
+        /// <summary>
+        /// Creates a new instance of the service.
+        /// </summary>
+        /// <param name="myShopifyUrl">The shop's *.myshopify.com URL.</param>
+        /// <param name="shopAccessToken">An API access token for the shop.</param>
+        public AccessScopeService(string myShopifyUrl, string shopAccessToken) : base(myShopifyUrl, shopAccessToken) { }
+
+        /// <summary>
+        /// Retrieves a list of access scopes associated to the access token.
+        /// </summary>
+        public virtual async Task<IEnumerable<AccessScope>> ListAsync()
+        {
+            var req = PrepareRequest($"oauth/access_scopes.json");
+            return await ExecuteRequestAsync<List<AccessScope>>(req, HttpMethod.Get, rootElement: "access_scopes");
+        }
+    }
+}
