70222cf09653b7bbe3a1cbb6e7d932ba42e13f2b
Joshua Harms <joshua@nozzlegear.com>
2019-01-21 16:39:27 -0600

    Implement missing CheckoutService endpoints, closes #220

---
+++ ShopifySharp/Entities/CheckoutShippingRatePrices.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+
+namespace ShopifySharp
+{
+    public class CheckoutShippingRatePrices
+    {
+        [JsonProperty("totalTax")]
+        public string TotalTax { get; set; }
+
+        [JsonProperty("totalPrice")]
+        public string TotalPrice { get; set; }
+
+        [JsonProperty("subtotalPrice")]
+        public string SubtotalPrice { get; set; }
+    }
+}
--- ShopifySharp/Services/Checkout/CheckoutService.cs
+++ ShopifySharp/Services/Checkout/CheckoutService.cs
@@ -9,7 +9,7 @@ using System;
 namespace ShopifySharp
 {
     /// <summary>
-    /// A service for manipulating Shopify orders.
+    /// A service for manipulating Shopify checkouts.
     /// </summary>
     public class CheckoutService : ShopifyService
     {
@@ -55,5 +55,57 @@ namespace ShopifySharp
             return await ExecuteRequestAsync<List<Checkout>>(req, HttpMethod.Get, rootElement: "checkouts");
         }
 
+        /// <summary>
+        /// Creates a new Checkout.
+        /// </summary>
+        public virtual async Task<Checkout> CreateAsync(Checkout checkout)
+        {
+            var req = PrepareRequest("checkout.json");
+            var body = checkout.ToDictionary();
+
+            return await ExecuteRequestAsync<Checkout>(req, HttpMethod.Post, new JsonContent(checkout), rootElement: "checkout");
+        }
+
+        /// <summary>
+        /// Completes a checkout without requiring payment.
+        /// </summary>
+        public virtual async Task<Checkout> CompleteAsync(string token)
+        {
+            var req = PrepareRequest($"checkouts/{token}/complete.json");
+
+            return await ExecuteRequestAsync<Checkout>(req, HttpMethod.Post, rootElement: "checkout");
+        }
+
+        /// <summary>
+        /// Gets an existing, processing or completed checkout.
+        /// </summary>
+        public virtual async Task<Checkout> GetAsync(string token)
+        {
+            var req = PrepareRequest($"checkouts/{token}.json");
+
+            return await ExecuteRequestAsync<Checkout>(req, HttpMethod.Get, rootElement: "checkout");
+        }
+
+        /// <summary>
+        /// Updates an existing checkout based on the token id.
+        /// </summary>
+        public virtual async Task<Checkout> UpdateAsync(string token, Checkout updatedCheckout)
+        {
+            var req = PrepareRequest($"checkouts/{token}.json");
+
+            return await ExecuteRequestAsync<Checkout>(req, HttpMethod.Put, new JsonContent(updatedCheckout), rootElement: "checkout");
+        }
+
+        /// <summary>
+        /// Retrieves a list of available shipping rates for the specified checkout. Implementers need to poll this endpoint until rates become available. Each shipping rate contains the checkout's 
+        /// new subtotal price, total tax, and total price in the event that this shipping rate is selected. This can be used to update the UI without performing further API requests. To apply a 
+        /// shipping rate, update the checkout's shipping line with the handle of the selected rate. 
+        /// </summary>
+        public virtual async Task<IEnumerable<CheckoutShippingRate>> ListShippingRatesAsync(string token)
+        {
+            var req = PrepareRequest($"checkouts/{token}/shipping_rates.json");
+
+            return await ExecuteRequestAsync<List<CheckoutShippingRate>>(req, HttpMethod.Get, rootElement: "shipping_rates");
+        }
     }
 }
