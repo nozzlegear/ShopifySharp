133b939a389dab9181bfab81a510ebc71be13435
Clement <clement.gutel@gmail.com>
2019-02-13 18:32:50 +1100

    Fixed minor bugs in Shopify payments filter classes.

--- ShopifySharp/Filters/ShopifyPaymentsTransactionFilter.cs
+++ ShopifySharp/Filters/ShopifyPaymentsTransactionFilter.cs
@@ -3,14 +3,8 @@ using System;
 
 namespace ShopifySharp.Filters 
 {
-    public class ShopifyPaymentsTransactionFilter : Parameterizable 
+    public class ShopifyPaymentsTransactionFilter : ListFilter
     {
-        /// <summary>
-        /// Filter response to transactions exclusively after the specified ID.
-        /// </summary>
-        [JsonProperty("since_id")]
-        public long? SinceId { get; set; }
-
         /// <summary>
         /// Filter response to transactions exclusively before the specified ID
         /// </summary>
@@ -21,13 +15,13 @@ namespace ShopifySharp.Filters
         /// Filter response to transactions placed in test mode.
         /// </summary>
         [JsonProperty("test")]
-        public bool Test { get; set; }
+        public bool? Test { get; set; }
 
         /// <summary>
         /// Filter response to transactions paid out in the specified payout.
         /// </summary>
         [JsonProperty("payout_id")]
-        public string PayoutId { get; set; }
+        public long? PayoutId { get; set; }
 
         /// <summary>
         /// Filter response to transactions with the specified payout status
