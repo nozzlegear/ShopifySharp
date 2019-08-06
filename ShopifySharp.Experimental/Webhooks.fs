fbec860736da4dbfdaed263d0e475fe171292c30
Joshua Harms <joshua@nozzlegear.com>
2019-08-05 21:31:46 -0500

    Improved usage of webhook builder

--- ShopifySharp.Experimental/Webhooks.fs
+++ ShopifySharp.Experimental/Webhooks.fs
@@ -1,32 +1,50 @@
 namespace ShopifySharp.Experimental
-open System.Collections.Generic
 
 module Webhooks = 
-    type private Properties = Map<string, PropertyValue>
-    let private add name value (properties: Properties) = 
+    type WebhookProperty = 
+        | Address 
+        | Fields 
+        | Format 
+        | MetafieldNamespaces 
+        | Topic 
+    with 
+        member x.ToJsonName = 
+            match x with 
+            | Address -> "address"
+            | Fields -> "fields"
+            | Format -> "format"
+            | MetafieldNamespaces -> "metafield_namespaces"
+            | Topic -> "topic"
+
+    type private WebhookProperties = Map<WebhookProperty, obj option>
+
+    let private add name value (properties: WebhookProperties): WebhookProperties = 
         properties
         |> Map.add name value 
 
-    let newWebhook: Properties = Map.empty
-    let address = add "address"
-    let fields = add "fields"
-    let format = add "format"
-    let metafieldNamespaces = add "metafield_namespaces"
-    let topic = add "topic"
-
-    // Current usage:
-    let private myWebhook = 
-        newWebhook
-        |> address (NotNull <| box "Some address")
-        |> format (NotNull <| box "json")
-        |> metafieldNamespaces Null
-        // Creates the following data:
-        // { address: "Some address", format: "json", metafield_namespaces: null }
-
-    // Ideal usage
-    // let private myWebhook = 
-    //      newWebhook
-    //      |> setValue Address "Some address"
-    //      |> setValue Format "json"
-    //      |> setNull MetafieldNamespaces
-    //      |> remove Topic // Remove does not set the value to null, it removes the entire value from the map so it is not sent at all
+    /// <summary>
+    /// Begins building a new webhook dictionary which can be serialized to JSON.
+    /// `let myWebhook = newWebhook |> address "https://example.com" |> topic "app/uninstalled" |> format "json"`
+    /// </summary>
+    let newWebhook : WebhookProperties = 
+        Map.empty
+    let address (s: string) = 
+        Some (box s) |> add Address
+    let fields (s: string seq) = 
+        Some (box s) |> add Fields
+    let format (s: string) = 
+        Some (box s) |> add Format 
+    let metafieldNamespaces (s: string seq) = 
+        Some (box s) |> add MetafieldNamespaces
+    let topic (s: string) = 
+        Some (box s) |> add Topic
+    /// <summary>
+    /// Explicitly sets the value of a property to null when serialized to JSON.
+    /// </summary>
+    let makePropertyNull topic = 
+        add topic None
+    /// <summary>
+    /// Removes a property entirely when the webhook is serialized to JSON. Not even null will be sent in its place.
+    /// </summary>
+    let removeProperty topic : WebhookProperties -> WebhookProperties = 
+        Map.remove topic
