ca63204fcf5d73450ef0192f693088ecbed71e10
Joshua Harms <joshua@nozzlegear.com>
2019-09-27 16:25:10 -0500

    Only build release artifacts when the current commit has been tagged

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,23 +1,39 @@
 #! /bin/pwsh
 
-$artifacts = "./artifacts";
-
 dotnet restore;
 dotnet build -c Release;
-
-# Get the prerelease version suffix from AppVeyor's build revision
-# Source: https://github.com/another-guy/Mirror/blob/9034cd0d6ee6ec072469f6c0f07bdb16b4b5764e/Build.ps1
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "b{0:D5}" -f [convert]::ToInt32($revision, 10)
 
 # Get a list of the projects that will be built and packed (those that aren't test projects)
 $projects = gci "./ShopifySharp*/*.*sproj" | where-object -filterscript { $_ -inotmatch "tests" } 
 
-# Pack the projects as both release and prerelease versions
-# TODO: use the following source to create a script that knows when to pack as release or prerelease
-# example script: https://github.com/another-guy/Mirror/blob/9034cd0d6ee6ec072469f6c0f07bdb16b4b5764e/Build.ps1
-$projects | % { dotnet pack --no-build -c Release -o $artifacts $_ }
-$projects | % { dotnet pack --no-build -c Release -o $artifacts --version-suffix $revision $_ }
+# Check if the latest commit has been tagged for release. 
+# If so, pack the project as release artifacts.
+# If not, pack the project as prerelease artifacts.
+# Source: https://github.com/another-guy/Mirror/blob/9034cd0d6ee6ec072469f6c0f07bdb16b4b5764e/Build.ps1
+$tagOfHead = iex 'git tag -l --contains HEAD'
+$artifacts = "./artifacts";
+
+if ([string]::IsNullOrEmpty($tagOfHead))
+{
+    # Get the prerelease version suffix from AppVeyor's build revision
+    $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+    $revision = "b{0:D5}" -f [convert]::ToInt32($revision, 10)
+    $projects | % { dotnet pack --no-build -c Release -o $artifacts --version-suffix $revision $_ }
+}
+else
+{
+    # Unused, but we could also compare that the VersionSuffix attributes of a project match the tag and refuse if they don't.
+    $projects | % {
+        [xml]$projectFile = Get-Content $_
+        $projectVersion = $projectFile.Project.PropertyGroup.VersionPrefix.Get(0)
+
+        if ($tagOfHead.Trim() -ne $projectVersion.Trim()) {
+            write-host "WARNING: Version $projectVersion of project $($_.Name) does not match git tag $tagOfHead." -ForegroundColor yellow
+        }
+    }
+    
+    $projects | % { dotnet pack --no-build -c Release -o $artifacts $_ }
+}
 
 # Gather all nuget packages and push them to AppVeyor artifacts
 gci "./ShopifySharp*/$artifacts/*.nupkg" | % {
