61854bd3cbd23f303fd93cc4bf33014e078ff69a
Joshua Harms <joshua@nozzlegear.com>
2020-02-13 15:04:45 -0600

    Remove obsolete/undocumented methods for counting/listing checkouts

--- ShopifySharp/Services/Checkout/CheckoutService.cs
+++ ShopifySharp/Services/Checkout/CheckoutService.cs
@@ -13,49 +13,9 @@ namespace ShopifySharp
     /// </summary>
     public class CheckoutService : ShopifyService
     {
-        /// <summary>
-        /// Creates a new instance of <see cref="OrderService" />.
-        /// </summary>
         /// <param name="myShopifyUrl">The shop's *.myshopify.com URL.</param>
         /// <param name="shopAccessToken">An API access token for the shop.</param>
         public CheckoutService(string myShopifyUrl, string shopAccessToken) : base(myShopifyUrl, shopAccessToken) { }
-
-        /// <summary>
-        /// Gets a count of all of the shop's abandoned checkouts.
-        /// </summary>
-        /// <param name="filter">Options for filtering the count.</param>
-        /// <returns>The count of all orders for the shop.</returns>
-        [Obsolete("This endpoint is an undocumented feature. Shopify may remove support for this endpoint at any time, without warning.")]
-        public virtual async Task<int> CountAsync(CheckoutFilter filter = null)
-        {
-            throw new NotImplementedException();
-            // var req = PrepareRequest("checkouts/count.json");
-            //
-            // if (filter != null)
-            // {
-            //     req.QueryParams.AddRange(filter.ToParameters());
-            // }
-            //
-            // return await ExecuteRequestAsync<int>(req, HttpMethod.Get, rootElement: "count");
-        }
-
-        /// <summary>
-        /// Gets a list of up to 250 of the shop's abandoned carts.
-        /// </summary>
-        /// <returns></returns>
-        [Obsolete("This endpoint is an undocumented feature. Shopify may remove support for this endpoint at any time, without warning.")]
-        public virtual async Task<IEnumerable<Checkout>> ListAsync(IListFilter filter)
-        {
-            throw new Exception("not yet implemented");
-            // var req = PrepareRequest("checkouts.json");
-            //
-            // if (options != null)
-            // {
-            //     req.QueryParams.AddRange(options.ToParameters());
-            // }
-            //
-            // return await ExecuteRequestAsync<List<Checkout>>(req, HttpMethod.Get, rootElement: "checkouts");
-        }
 
         /// <summary>
         /// Creates a new Checkout.
