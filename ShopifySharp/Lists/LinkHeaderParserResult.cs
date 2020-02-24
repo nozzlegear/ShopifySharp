08de465cd3b465a2e65563bad12de5ee5a791ed1
Clement <clement.gutel@gmail.com>
2020-02-24 12:36:35 +0100

    -Extracted link header parsing logic into its own class -Added tests for link header parsing -Handling extra cases such as the presence of commas in the link url when the 'fields' parameter is present

---
+++ ShopifySharp/Lists/LinkHeaderParserResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopifySharp.Lists
+{
+    public class LinkHeaderParseResult
+    {
+        public class PagingLink
+        {
+            public string Url { get; }
+
+            public string PageInfo { get; }
+
+            public PagingLink(string url, string pageInfo)
+            {
+                Url = url;
+                PageInfo = pageInfo;
+            }
+        }
+
+        public PagingLink PreviousLink { get; }
+
+        public PagingLink NextLink { get; }
+
+        public LinkHeaderParseResult(PagingLink previousLink, PagingLink nextLink)
+        {
+            PreviousLink = previousLink;
+            NextLink = nextLink;
+        }
+    }
+}
