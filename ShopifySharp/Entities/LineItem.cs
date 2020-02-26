d7d413a154ae85afa3029a660f477c55b0003e1d
Clement <clement.gutel@gmail.com>
2020-02-26 17:23:44 +0100

    Adding remark about custom converter for lineItem.Properties.

--- ShopifySharp/Entities/LineItem.cs
+++ ShopifySharp/Entities/LineItem.cs
@@ -150,6 +150,9 @@ namespace ShopifySharp
         /// Often used to provide product customization options.
         /// An array of <see cref="TaxLine"/> objects, each of which details the taxes applicable to this <see cref="LineItem"/>.
         /// </summary>
+        /// <remarks>
+        /// See https://github.com/nozzlegear/ShopifySharp/pull/461 for why the custom converter is required
+        /// </remarks>
         [JsonProperty("properties")]
         [JsonConverter(typeof(NullOnErrorConverter))]
         public IEnumerable<LineItemProperty> Properties { get; set; }
