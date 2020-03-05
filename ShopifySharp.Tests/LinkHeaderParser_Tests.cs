ef15da5ac3824d1fbe40a44205d34b397d858375
Joshua Harms <joshua@nozzlegear.com>
2020-03-05 14:47:58 -0600

    Add test for parsing fields from link header

--- ShopifySharp.Tests/LinkHeaderParser_Tests.cs
+++ ShopifySharp.Tests/LinkHeaderParser_Tests.cs
@@ -92,5 +92,25 @@ namespace ShopifySharp.Tests
             Assert.Equal(res.NextLink.Url, "https://test.myshopify.com/admin/api/2019-07/products.json?limit=3&page_info=opqrstu");
             Assert.Equal(res.NextLink.PageInfo, "opqrstu");
         }
+
+        [Fact]
+        public void Parses_Fields_Parameter()
+        {
+            var res = LinkHeaderParser.Parse<Product>("<https://test.myshopify.com/admin/api/2019-07/products.json?limit=3&page_info=abcdefg&fields=id%2Ccustomer%2Ccreated_at>; rel=\"previous\", <https://test.myshopify.com/admin/api/2019-07/products.json?limit=3&page_info=opqrstu&fields=id%2Ccustomer%2Ccreated_at>; rel=\"next\"");
+            Assert.NotNull(res.PreviousLink);
+            Assert.NotNull(res.NextLink);
+            Assert.Equal("https://test.myshopify.com/admin/api/2019-07/products.json?limit=3&page_info=abcdefg&fields=id%2Ccustomer%2Ccreated_at", res.PreviousLink.Url);
+            Assert.Equal("id,customer,created_at", res.PreviousLink.Fields);
+            Assert.Equal("https://test.myshopify.com/admin/api/2019-07/products.json?limit=3&page_info=opqrstu&fields=id%2Ccustomer%2Ccreated_at", res.NextLink.Url);
+            Assert.Equal("id,customer,created_at", res.NextLink.Fields);
+
+            var nextPageFilter = res.NextLink.GetFollowingPageFilter();
+            
+            Assert.Equal("id,customer,created_at", nextPageFilter.Fields);
+
+            var previousPageFilter = res.PreviousLink.GetFollowingPageFilter();
+            
+            Assert.Equal("id,customer,created_at", previousPageFilter.Fields);
+        }
     }
 }
