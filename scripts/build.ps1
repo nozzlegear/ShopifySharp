b747eafd3c40aa34d6acef5d79efda194637a5fd
Joshua Harms <joshua@nozzlegear.com>
2020-04-20 15:19:35 -0500

    Pack for both release and prerelease on every build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -30,41 +30,37 @@ exec { & dotnet restore --verbosity quiet }
 # Get a list of the projects that will be built and packed (those that aren't test projects)
 $projects = gci "./ShopifySharp*/*.*sproj" | where-object -filterscript { $_ -inotmatch "tests" }
 
-# Check if the latest commit has been tagged for release. 
-# If so, pack the project as release artifacts.
-# If not, pack the project as prerelease artifacts.
-# Source: https://github.com/another-guy/Mirror/blob/9034cd0d6ee6ec072469f6c0f07bdb16b4b5764e/Build.ps1
-$tagOfHead = iex 'git tag -l --contains HEAD'
+# Pack both prerelease and release artifacts. 
+# The appveyor.yml script will automatically publish prerelease packages to nuget.
+# Release packages can be manually published.
 $artifactsFolder = "./artifacts";
 
-if ([string]::IsNullOrEmpty($tagOfHead))
-{
-    write-host "============================= PACKING FOR PRERELEASE ==========================="
-    # Get the prerelease version suffix from AppVeyor's build revision
-    $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-    $revision = "b{0:D5}" -f [convert]::ToInt32($revision, 10)
-    $projects | % { 
-        exec { & dotnet pack -c Release -o $artifactsFolder --version-suffix $revision $_ }
-        write-host "Packed $($_.Name) for prerelease." -ForegroundColor "green"
+write-host "============================= PACKING PRERELEASE PACKAGES ==========================="
+# Get the prerelease version suffix from AppVeyor's build revision
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "b{0:D5}" -f [convert]::ToInt32($revision, 10)
+$projects | % { 
+    exec { & dotnet pack -c Release -o $artifactsFolder --version-suffix $revision $_ }
+    write-host "Packed $($_.Name) for prerelease." -ForegroundColor "green"
+}
+
+# Source for getting git tag: https://github.com/another-guy/Mirror/blob/9034cd0d6ee6ec072469f6c0f07bdb16b4b5764e/Build.ps1
+$tagOfHead = iex 'git tag -l --contains HEAD'
+
+write-host "============================= PACKING RELEASE PACKAGES ==========================="
+# Unused, but we could also compare that the VersionSuffix attributes of a project match the tag and refuse if they don't.
+$projects | % {
+    [xml]$projectFile = Get-Content $_
+    $projectVersion = $projectFile.Project.PropertyGroup.VersionPrefix.Get(0)
+
+    if ($tagOfHead.Trim() -ne $projectVersion.Trim()) {
+        write-host "WARNING: Version $projectVersion of project $($_.Name) does not match git tag $tagOfHead." -ForegroundColor yellow
     }
 }
-else
-{
-    write-host "============================= PACKING FOR RELEASE ==========================="
-    # Unused, but we could also compare that the VersionSuffix attributes of a project match the tag and refuse if they don't.
-    $projects | % {
-        [xml]$projectFile = Get-Content $_
-        $projectVersion = $projectFile.Project.PropertyGroup.VersionPrefix.Get(0)
 
-        if ($tagOfHead.Trim() -ne $projectVersion.Trim()) {
-            write-host "WARNING: Version $projectVersion of project $($_.Name) does not match git tag $tagOfHead." -ForegroundColor yellow
-        }
-    }
-
-    $projects | % { 
-        exec { & dotnet pack -c Release -o $artifactsFolder $_ }
-        write-host "Packed $($_.Name) for release." -ForegroundColor "green"
-    }
+$projects | % { 
+    exec { & dotnet pack -c Release -o $artifactsFolder $_ }
+    write-host "Packed $($_.Name) for release." -ForegroundColor "green"
 }
 
 write-host "============================= PUSHING APPVEYOR ARTIFACTS ==========================="
