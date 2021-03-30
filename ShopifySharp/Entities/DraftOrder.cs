8804add0b4439a245098925af05c063b6f272d44
Clement <clement.gutel@gmail.com>
2021-03-30 13:41:11 +0200

    Adding new property tax_exemptions to draft order

--- ShopifySharp/Entities/DraftOrder.cs
+++ ShopifySharp/Entities/DraftOrder.cs
@@ -97,6 +97,12 @@ namespace ShopifySharp
         public bool? TaxExempt { get; set; }
 
         /// <summary>
+        /// Whether the customer is exempt from paying specific taxes on their order
+        /// </summary>
+        [JsonProperty("tax_exemptions")]
+        public string[] TaxExemptions { get; set; }
+
+        /// <summary>
         /// An array of tax_line objects, each of which details the total taxes applicable to the order. When creating an order through the API, tax lines may be specified on the order or the line items but not both. Tax lines specified on the order are split on the taxable line items in the created order. 
         /// </summary>
         [JsonProperty("tax_lines")]
