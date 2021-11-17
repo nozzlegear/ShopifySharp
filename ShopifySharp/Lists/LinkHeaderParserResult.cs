b98211a36fe67fe736f2f0279a9f311c0e36f3fb
Joshua Harms <joshua@nozzlegear.com>
2021-11-17 14:56:27 -0600

    Move PagingLink to its own class/file

--- ShopifySharp/Lists/LinkHeaderParserResult.cs
+++ ShopifySharp/Lists/LinkHeaderParserResult.cs
@@ -4,30 +4,6 @@ namespace ShopifySharp.Lists
 {
     public class LinkHeaderParseResult<T>
     {
-        public class PagingLink<T>
-        {
-            public string Url { get; }
-
-            public int? Limit { get; }
-
-            public string PageInfo { get; }
-            
-            public string Fields { get; }
-
-            public PagingLink(string url, string pageInfo, int? limit, string fields = null)
-            {
-                Url = url;
-                PageInfo = pageInfo;
-                Limit = limit;
-                Fields = fields;
-            }
-
-            public ListFilter<T> GetFollowingPageFilter(int? limit = null, string fields = null)
-            {
-                return new ListFilter<T>(this.PageInfo, limit ?? this.Limit, fields ?? this.Fields);
-            }
-        }
-
         public PagingLink<T> PreviousLink { get; }
 
         public PagingLink<T> NextLink { get; }
