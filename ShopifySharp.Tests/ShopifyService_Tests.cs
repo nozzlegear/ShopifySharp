e93e2ad2840824715b0a852e28227eff58dd84cd
Joshua Harms <joshua@nozzlegear.com>
2022-03-08 13:42:49 -0600

    Add tests for ShopifyService.SetHttpClient and ShopifyService.SetGlobalHttpClientFactory

--- ShopifySharp.Tests/ShopifyService_Tests.cs
+++ ShopifySharp.Tests/ShopifyService_Tests.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using ShopifySharp.Filters;
 using Xunit;
 
 namespace ShopifySharp.Tests
@@ -332,6 +335,91 @@ namespace ShopifySharp.Tests
             json = "{\"errors_foo\":\"bar\"}";
             
             Assert.False(ShopifyService.TryParseErrorJson(json, out _));
+        }
+
+        [Fact(DisplayName = "Allows setting an instance-specific HttpClient")]
+        public async Task Allows_Setting_Instance_Specific_HttpClient()
+        {
+            var service = new ShopService(Utils.MyShopifyUrl, Utils.AccessToken);
+            service.SetExecutionPolicy(new LeakyBucketExecutionPolicy());
+            
+            // Set the client
+            var client = new FakeHttpClient();
+            service.SetHttpClient(client);
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => service.GetAsync());
+            
+            Assert.Equal("This is an exception thrown by the FakeHttpClient", ex.Message);
+            
+            // Remove the client
+            service.SetHttpClient(null);
+
+            var shop = await service.GetAsync();
+            
+            Assert.NotNull(shop);
+        }
+
+        [Fact(DisplayName = "Allows setting a global HttpClientFactory")]
+        public async Task Allows_Setting_Global_HttpClientFactory()
+        {
+            var factory = new FakeHttpClientFactory();
+            
+            ShopifyService.SetGlobalHttpClientFactory(factory);
+
+            var shopService = new ShopService(Utils.MyShopifyUrl, Utils.AccessToken);
+            var orderService = new OrderService(Utils.MyShopifyUrl, Utils.AccessToken);
+            var policy = new LeakyBucketExecutionPolicy();
+            var orderFilter = new OrderListFilter
+            {
+                Limit = 1
+            };
+            
+            shopService.SetExecutionPolicy(policy);
+            orderService.SetExecutionPolicy(policy);
+            
+            var ex1 = await Assert.ThrowsAsync<Exception>(() => shopService.GetAsync());
+            var ex2 = await Assert.ThrowsAsync<Exception>(() => orderService.ListAsync(orderFilter));
+            
+            Assert.Equal("This is an exception thrown by the FakeHttpClient", ex1.Message);
+            Assert.Equal("This is an exception thrown by the FakeHttpClient", ex2.Message);
+            
+            // Removing the factory should only remove it for future instances
+            ShopifyService.SetGlobalHttpClientFactory(null);
+            
+            ex1 = await Assert.ThrowsAsync<Exception>(() => shopService.GetAsync());
+            ex2 = await Assert.ThrowsAsync<Exception>(() => orderService.ListAsync(orderFilter));
+            
+            Assert.Equal("This is an exception thrown by the FakeHttpClient", ex1.Message);
+            Assert.Equal("This is an exception thrown by the FakeHttpClient", ex2.Message);
+            
+            // Instantiating the services again should now use the default HttpClientFactory
+            shopService = new ShopService(Utils.MyShopifyUrl, Utils.AccessToken);
+            orderService = new OrderService(Utils.MyShopifyUrl, Utils.AccessToken);
+            
+            shopService.SetExecutionPolicy(policy);
+            orderService.SetExecutionPolicy(policy);
+
+            var shop = await shopService.GetAsync();
+            var orders = await orderService.ListAsync(orderFilter);
+            
+            Assert.NotNull(shop);
+            Assert.NotNull(orders);
+        }
+
+        class FakeHttpClient : HttpClient
+        {
+            public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                throw new Exception("This is an exception thrown by the FakeHttpClient");
+            }
+        }
+
+        class FakeHttpClientFactory : IHttpClientFactory
+        {
+            public HttpClient CreateClient(string name)
+            {
+                return new FakeHttpClient();
+            }
         }
     }
 }
