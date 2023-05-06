41d4bc890da6cb91d89a84268fa108641c3c5bbb
Micah Bredenhorst <degrootmicah@gmail.com>
2023-05-06 12:04:21 +0200

    Added the missing Shopify Return object to the Shopify Refund object

---
+++ ShopifySharp/Entities/Return.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopifySharp.Entities
+{
+    /// <summary>
+    /// An object representing the Shopify Return
+    /// </summary>
+    public class Return : ShopifyObject
+    {
+    }
+}
