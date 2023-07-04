4fa689c6fbaacf5562d4f943eb6986c970046e8b
Anderson Bueno <anderson@mxdata.com>
2023-07-04 18:14:18 -0300

    Mapping the class to match the JSON and deserialize in a correct way

--- ShopifySharp/Entities/LineItemDuty.cs
+++ ShopifySharp/Entities/LineItemDuty.cs
@@ -13,11 +13,8 @@ namespace ShopifySharp
         [JsonProperty("country_code_of_origin")]
         public string CountryCodeOfOrigin { get; set; }
 
-        [JsonProperty("shop_money")]
-        public Price ShopMoney { get; set; }
-
-        [JsonProperty("presentment_money")]
-        public Price PresentmentMoney { get; set; }
+        [JsonProperty("price_set")]
+        public PriceSet PriceSet { get; set; }
 
         [JsonProperty("tax_lines")]
         public IEnumerable<TaxLine> TaxLines { get; set; }
