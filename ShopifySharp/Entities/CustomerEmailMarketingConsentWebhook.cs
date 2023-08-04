c3ebe677fa195e0aaa6cb4e8fea9adf0870745f1
Clement <clement.gutel@gmail.com>
2023-08-04 13:38:17 +1000

    Added webhook payload object for topic 'customers_email_marketing_consent/update'

---
+++ ShopifySharp/Entities/CustomerEmailMarketingConsentWebhook.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+
+namespace ShopifySharp
+{
+    /// <summary>
+    /// Payload for webhook customers_email_marketing_consent/update
+    /// https://shopify.dev/docs/api/admin-rest/2023-07/resources/webhook#event-topics-customers-marketing-consent-update
+    /// </summary>
+    public class CustomerEmailMarketingConsentWebhook : ShopifyObject
+    {
+        [JsonProperty("email_address")]
+        public string EmailAddress { get; set; }
+
+        [JsonProperty("email_marketing_consent")]
+        public CustomerEmailMarketingConsent EmailMarketingConsent { get; set; }
+    }
+}
--- ShopifySharp/Entities/CustomerMarketingConsentWebhook.cs
+++ ShopifySharp/Entities/CustomerMarketingConsentWebhook.cs
@@ -2,6 +2,10 @@ using Newtonsoft.Json;
 
 namespace ShopifySharp
 {
+    /// <summary>
+    /// Payload for webhook customers_marketing_consent/update
+    /// https://shopify.dev/docs/api/admin-rest/2023-07/resources/webhook#event-topics-customers-marketing-consent-update
+    /// </summary>
     public class CustomerMarketingConsentWebhook : ShopifyObject
     {
         [JsonProperty("phone")]
