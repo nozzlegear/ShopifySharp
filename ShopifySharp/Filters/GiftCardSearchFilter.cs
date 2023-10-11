e3ba2d6027c36c77874159cd6b07819ca01cc6e6
Clement <clement.gutel@gmail.com>
2023-10-11 17:19:02 +1100

    Adding gift search filter parameters

--- ShopifySharp/Filters/GiftCardSearchFilter.cs
+++ ShopifySharp/Filters/GiftCardSearchFilter.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System;
 
 namespace ShopifySharp.Filters
 {
@@ -16,5 +17,29 @@ namespace ShopifySharp.Filters
         /// </summary>
         [JsonProperty("query")]
         public string Query { get; set; }
+
+        /// <summary>
+        /// Restricts results to those created after date (format: 2008-12-31 03:00).
+        /// </summary>
+        [JsonProperty("created_at_min")]
+        public DateTimeOffset? CreatedAtMin { get; set; }
+
+        /// <summary>
+        /// Restricts results to those created before date (format: 2008-12-31 03:00).
+        /// </summary>
+        [JsonProperty("created_at_max")]
+        public DateTimeOffset? CreatedAtMax { get; set; }
+
+        /// <summary>
+        /// Restricts results to those last updated after date (format: 2008-12-31 03:00).
+        /// </summary>
+        [JsonProperty("updated_at_min")]
+        public DateTimeOffset? UpdatedAtMin { get; set; }
+
+        /// <summary>
+        /// Restricts results to those last updated before date (format: 2008-12-31 03:00).
+        /// </summary>
+        [JsonProperty("updated_at_max")]
+        public DateTimeOffset? UpdatedAtMax { get; set; }
     }
 }
