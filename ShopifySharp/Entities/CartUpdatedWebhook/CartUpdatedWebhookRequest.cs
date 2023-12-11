16e101573ccd6136c3a835ca72cac0dc775b967b
Joshua Harms <joshua@nozzlegear.com>
2023-12-11 00:09:31 -0600

    Update model properties

--- ShopifySharp/Entities/CartUpdatedWebhook/CartUpdatedWebhookRequest.cs
+++ ShopifySharp/Entities/CartUpdatedWebhook/CartUpdatedWebhookRequest.cs
@@ -1,24 +1,28 @@
+#nullable enable
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace ShopifySharp.Entities.CartUpdatedWebhook
 {
-    public class CartUpdatedWebhookRequest : ShopifyObject
+    public class CartUpdatedWebhookRequest
     {
-        [JsonProperty("token")]
-        public string Token { get; set; }
+        [JsonProperty("created_at")]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; } = "";
 
         [JsonProperty("line_items")]
-        public IEnumerable<CartUpdatedWebhookLineItem> LineItems { get; set; }
+        public ICollection<CartUpdatedWebhookLineItem> LineItems { get; set; } = new List<CartUpdatedWebhookLineItem>();
 
         [JsonProperty("note")]
-        public string Note { get; set; }
+        public string Note { get; set; } = "";
+
+        [JsonProperty("token")]
+        public string Token { get; set; } = "";
 
         [JsonProperty("updated_at")]
         public DateTimeOffset? UpdatedAt { get; set; }
-
-        [JsonProperty("created_at")]
-        public DateTimeOffset? CreatedAt { get; set; }
     }
 }
