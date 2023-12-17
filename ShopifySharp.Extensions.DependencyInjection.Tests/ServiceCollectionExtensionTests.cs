e9c9e88e9bc6b79c5541c96da3be9e22448ab34d
Joshua Harms <joshua@nozzlegear.com>
2023-12-17 01:15:15 -0600

    Add services.AddShopifySharpUtilities

--- ShopifySharp.Extensions.DependencyInjection.Tests/ServiceCollectionExtensionTests.cs
+++ ShopifySharp.Extensions.DependencyInjection.Tests/ServiceCollectionExtensionTests.cs
@@ -1,3 +1,4 @@
+using ShopifySharp.Utilities;
 
 namespace ShopifySharp.Extensions.DependencyInjection.Tests;
 
@@ -20,6 +21,33 @@ public class ServiceCollectionExtensionTests
             .NotBeNull()
             .And
             .BeOfType<TestRequestExecutionPolicy>();
+    }
+
+    [Fact]
+    public void AddShopifySharpUtilities_AddsUtilities()
+    {
+        // Setup
+        var container = new ServiceCollection();
+
+        // Act
+        var serviceProvider = container.AddShopifySharpUtilities().BuildServiceProvider();
+
+        // Assert
+        serviceProvider.GetService<IShopifyDomainUtility>()
+            .Should()
+            .NotBeNull()
+            .And.BeOfType<ShopifyDomainUtility>()
+            .And.BeAssignableTo<IShopifyDomainUtility>();
+        serviceProvider.GetService<IShopifyOauthUtility>()
+            .Should()
+            .NotBeNull()
+            .And.BeOfType<ShopifyOauthUtility>()
+            .And.BeAssignableTo<IShopifyOauthUtility>();
+        serviceProvider.GetService<IShopifyRequestValidationUtility>()
+            .Should()
+            .NotBeNull()
+            .And.BeOfType<ShopifyRequestValidationUtility>()
+            .And.BeAssignableTo<IShopifyRequestValidationUtility>();
     }
 
     [Fact]
