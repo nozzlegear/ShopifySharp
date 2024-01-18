2821fc180a0b7d88d4267b8153a74b4dfb4d46e1
Joshua Harms <joshua@nozzlegear.com>
2024-01-18 08:53:10 -0600

    Use file-scoped namespaces

--- ShopifySharp.Tests/Partner_Tests.cs
+++ ShopifySharp.Tests/Partner_Tests.cs
@@ -1,77 +1,79 @@
 using Newtonsoft.Json.Linq;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
+using ShopifySharp.Infrastructure;
 using Xunit;
 
-namespace ShopifySharp.Tests
+namespace ShopifySharp.Tests;
+
+[Trait("Category", "Partners")]
+public class Partner_Tests
 {
-    [Trait("Category", "Partners")]
-    public class Partner_Tests
+    private async Task<JToken> Query(string query, IRequestExecutionPolicy policy = null)
     {
-        private async Task<JToken> Query(string query, IRequestExecutionPolicy policy = null)
-        {
-            var svc = new PartnerService(Utils.OrganizationId, Utils.OrganizationToken);
+        var svc = new PartnerService(Utils.OrganizationId, Utils.OrganizationToken);
 
-            if (policy != null)
-                svc.SetExecutionPolicy(policy);
+        if (policy != null)
+            svc.SetExecutionPolicy(policy);
 
-            return await svc.PostAsync(query);
-        }
-
-        private async Task<JToken> QueryTransactions(IRequestExecutionPolicy policy = null)
-        {
-            string query = @"
-{
-  transactions
-  {
-    edges
-    {
-      node
-      {
-        id
-        createdAt
-      }
+        return await svc.PostAsync(query);
     }
-  }
-}";
-            return await Query(query, policy);
-        }
 
-        [Fact]
-        public async Task Transactions()
+    private async Task<JToken> QueryTransactions(IRequestExecutionPolicy policy = null)
+    {
+        const string query = """
+                             {
+                               transactions
+                               {
+                                 edges
+                                 {
+                                   node
+                                   {
+                                     id
+                                     createdAt
+                                   }
+                                 }
+                               }
+                             }
+                             """;
+        return await Query(query, policy);
+    }
+
+    [Fact]
+    public async Task Transactions()
+    {
+        var res = await QueryTransactions();
+        Assert.NotNull(res);
+    }
+
+    [Fact]
+    public async Task InvalidQueryMustThrowException()
+    {
+        const string graphQuery = "{{ query }";
+
+        var act = () => Query(graphQuery);
+
+        await act.Should().ThrowExactlyAsync<ShopifyHttpException>();
+    }
+
+    [Fact]
+    public async Task LeakyBucketPolicyShouldRetry()
+    {
+        var policy = new LeakyBucketExecutionPolicy();
+        bool caught = false;
+
+        try
         {
-            var res = await QueryTransactions();
-            Assert.NotNull(res);
+            //issue 10 parallel requests to trip the 4 req/sec limit
+            //they will be retried internally
+            await Task.WhenAll(Enumerable.Range(0, 10).Select(async _ => await QueryTransactions(policy)));
         }
-
-        [Fact]
-        public async Task InvalidQueryMustThrowException()
+        catch (ShopifyRateLimitException)
         {
-            const string graphQuery = "{{ query }";
-
-            var act = () => Query(graphQuery);
-
-            await act.Should().ThrowExactlyAsync<ShopifyHttpException>();
+            caught = true;
         }
 
-        [Fact]
-        public async Task LeakyBucketPolicyShouldRetry()
-        {
-            var policy = new LeakyBucketExecutionPolicy();
-            bool caught = false;
-
-            try
-            {
-                //issue 10 parallel requests to trip the 4 req/sec limit
-                //they will be retried internally
-                await Task.WhenAll(Enumerable.Range(0, 10).Select(async _ => await QueryTransactions(policy)));
-            }
-            catch (ShopifyRateLimitException)
-            {
-                caught = true;
-            }
-
-            Assert.False(caught);
-        }
+        Assert.False(caught);
     }
 }
