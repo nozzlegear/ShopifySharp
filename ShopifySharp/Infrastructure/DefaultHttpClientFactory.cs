d422fe1719e9186542c3b5dc615764f8126b4072
Joshua Harms <joshua@nozzlegear.com>
2024-01-26 10:20:59 -0600

    Remove deprecated DefaultHttpClientFactory

--- ShopifySharp/Infrastructure/DefaultHttpClientFactory.cs
+++ ShopifySharp/Infrastructure/DefaultHttpClientFactory.cs
@@ -4,21 +4,6 @@ using System.Net.Http;
 namespace ShopifySharp.Infrastructure
 {
     /// <summary>
-    /// ShopifySharp's default <see cref="IHttpClientFactory" />, which uses a static <see cref="HttpClient" />.
-    /// </summary>
-    // TODO: remove this class in a future release (6.4+)
-    [Obsolete("This class will be removed in a future version of ShopifySharp. It ignores all configurations and therefore has the potential to introduce subtle bugs when used outside of ShopifySharp.")]
-    public class DefaultHttpClientFactory : IHttpClientFactory
-    {
-        private static readonly HttpClient _Client = new HttpClient();
-
-        public HttpClient CreateClient(string name)
-        {
-            return _Client;
-        }
-    }
-
-    /// <summary>
     /// ShopifySharp's internal <see cref="IHttpClientFactory" />, which uses a static <see cref="HttpClient" />.
     /// </summary>
     internal class InternalHttpClientFactory : IHttpClientFactory
