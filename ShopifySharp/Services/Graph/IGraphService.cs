28bef32cf49f9d9e3792055b5bf940788fdb265f
Alejandro de Arriba <adearriba@outlook.com>
2024-02-08 19:15:13 +0100

    Update IGraphService.cs
    Added SendAsync methods that were missing.

--- ShopifySharp/Services/Graph/IGraphService.cs
+++ ShopifySharp/Services/Graph/IGraphService.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
@@ -29,5 +30,53 @@ namespace ShopifySharp
         /// <param name="cancellationToken">Cancellation Token</param>
         /// <returns>A JToken containing the data from the request.</returns>
         Task<JToken> PostAsync(JToken body, int? graphqlQueryCost = null, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Executes a Graph API Call.
+        /// </summary>
+        /// <param name="request">The query you would like to execute, as a <seealso cref="GraphRequest"/>. Please see documentation for formatting.</param>
+        /// <param name="graphqlQueryCost">
+        /// The requestedQueryCost available at extensions.cost.requestedQueryCost.
+        /// While it is optional, it is recommended to provide it to avoid wasting resources to issue API calls that will be throttled
+        /// </param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>A JsonElement containing the data from the request.</returns>
+        Task<JsonElement> SendAsync(GraphRequest request, int? graphqlQueryCost = null, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Executes a Graph API Call.
+        /// </summary>
+        /// <param name="request">The query you would like to execute, as a string. Please see documentation for formatting.</param>
+        /// <param name="graphqlQueryCost">
+        /// The requestedQueryCost available at extensions.cost.requestedQueryCost.
+        /// While it is optional, it is recommended to provide it to avoid wasting resources to issue API calls that will be throttled
+        /// </param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>A JsonElement containing the data from the request.</returns>
+        Task<JsonElement> SendAsync(string graphqlQuery, int? graphqlQueryCost = null, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Executes a Graph API Call.
+        /// </summary>
+        /// <param name="request">The query you would like to execute, as a <seealso cref="GraphRequest"/>. Please see documentation for formatting.</param>
+        /// <param name="graphqlQueryCost">
+        /// The requestedQueryCost available at extensions.cost.requestedQueryCost.
+        /// While it is optional, it is recommended to provide it to avoid wasting resources to issue API calls that will be throttled
+        /// </param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>A <see cref="TResult"/> containing the data from the request.</returns>
+        Task<TResult> SendAsync<TResult>(GraphRequest request, int? graphqlQueryCost = null, CancellationToken cancellationToken = default) where TResult : class;
+
+        /// <summary>
+        /// Executes a Graph API Call.
+        /// </summary>
+        /// <param name="request">The query you would like to execute, as a string. Please see documentation for formatting.</param>
+        /// <param name="graphqlQueryCost">
+        /// The requestedQueryCost available at extensions.cost.requestedQueryCost.
+        /// While it is optional, it is recommended to provide it to avoid wasting resources to issue API calls that will be throttled
+        /// </param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>A <see cref="TResult"/> containing the data from the request.</returns>
+        Task<TResult> SendAsync<TResult>(string graphqlQuery, int? graphqlQueryCost = null, CancellationToken cancellationToken = default) where TResult : class;
     }
 }
