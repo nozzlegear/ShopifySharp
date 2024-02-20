7e3cc731a1b58ddbfe5c844295be8c0846851039
David Sargent <david.sargent@basecommerce.com>
2024-02-20 16:49:51 -0600

    Add Shopify Payments Dispute Evidence Entities

---
+++ ShopifySharp/Entities/ShopifyPaymentsDisputeEvidenceBillingAddress.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifySharp
+{
+	public class ShopifyPaymentsDisputeEvidenceAddress : ShopifyObject
+	{
+		/// <summary>
+		/// The street address of the shipping / billing location.
+		/// </summary>
+		[JsonProperty("address1")]
+		public string Address1 { get; set; }
+
+		/// <summary>
+		/// The second line of the address. Typically the number of the apartment, suite, or unit.
+		/// </summary>
+		[JsonProperty("address2")]
+		public string Address2 { get; set; }
+
+		/// <summary>
+		/// The city of the shipping / billing location.
+		/// </summary>
+		[JsonProperty("city")]
+		public string City { get; set; }
+
+		/// <summary>
+		/// The zip code of the shipping / billing location.
+		/// </summary>
+		[JsonProperty("zip")]
+		public string Zip { get; set; }
+
+		/// <summary>
+		/// (Required) The two-letter country code (ISO 3166-1 alpha-2 format) of the shipping / billing location.
+		/// </summary>
+		[JsonProperty("country_code")]
+		public string CountryCode { get; set; }
+
+		/// <summary>
+		/// The province of the shipping / billing location.
+		/// </summary>
+		[JsonProperty("province")]
+		public string Province { get; set; }
+
+		/// <summary>
+		/// The province code of the shipping / billing location.
+		/// </summary>
+		[JsonProperty("province_code")]
+		public string ProvinceCode { get; set; }
+	}
+}
---
+++ ShopifySharp/Entities/ShopifyPaymentsDisputeEvidenceFulfillment.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifySharp
+{
+	public class ShopifyPaymentsDisputeEvidenceFulfillment 
+	{
+		[JsonProperty("shipping_carrier")]
+		public string ShippingCarrier { get; set; }
+
+		[JsonProperty("shipping_tracking_number")]
+		public long? ShippingTrackingNumber { get; set; }
+
+		[JsonProperty("shipping_date")]
+		public DateTimeOffset? ShippingDate { get; set; }
+	}
+}
