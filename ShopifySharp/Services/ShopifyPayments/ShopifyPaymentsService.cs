7b06c452a02400a66eac191f135de85157cefcfe
David Sargent <david.sargent@basecommerce.com>
2024-02-21 18:18:37 -0600

    Formatting fix

--- ShopifySharp/Services/ShopifyPayments/ShopifyPaymentsService.cs
+++ ShopifySharp/Services/ShopifyPayments/ShopifyPaymentsService.cs
@@ -72,11 +72,11 @@ namespace ShopifySharp
 		/// <inheritdoc />
 		public virtual async Task<ListResult<ShopifyPaymentsTransaction>> ListTransactionsAsync(ShopifyPaymentsTransactionListFilter filter = null, CancellationToken cancellationToken = default) =>
 			await ListTransactionsAsync(filter?.AsListFilter(), cancellationToken);
-		
+
 		/// <inheritdoc /> //
 		public async Task<ShopifyPaymentsDisputeEvidence> GetDisputeEvidenceAsync(long disputeId, CancellationToken cancellationToken = default) =>
 			await ExecuteGetAsync<ShopifyPaymentsDisputeEvidence>($"shopify_payments/disputes/{disputeId}/dispute_evidences.json", "dispute_evidence", cancellationToken: cancellationToken);
-		
+
 		/// <inheritdoc /> //
 		public async Task<ShopifyPaymentsDisputeEvidence> UpdateDisputeEvidenceAsync(long disputeId, ShopifyPaymentsDisputeEvidenceUpdate updateRequest, CancellationToken cancellationToken = default) =>
 			await ExecutePutAsync<ShopifyPaymentsDisputeEvidence>($"shopify_payments/disputes/{disputeId}/dispute_evidences.json", "dispute_evidence", cancellationToken: cancellationToken, updateRequest);
