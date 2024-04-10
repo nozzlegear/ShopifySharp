f6dac514aec1fc65e002e893826a676b3be7d694
Joshua Harms <joshua@nozzlegear.com>
2024-04-09 22:52:42 -0500

    Allow dev to disable error handling in GraphService
    Type: feature

--- ShopifySharp/Services/Graph/GraphRequest.cs
+++ ShopifySharp/Services/Graph/GraphRequest.cs
@@ -47,5 +47,10 @@ public class GraphRequest
     /// What you estimate this query will cost. Use this value to provide an estimated query cost to any <see cref="IRequestExecutionPolicy" />, to avoid sending a request that will be throttled. This value can be found in the query response data at <c>extensions.cost.requestedQueryCost</c>.
     /// </summary>
     public int? EstimatedQueryCost { get; set; }
+
+    /// <summary>
+    /// Tells the service whether it should throw an exception or not when it detects a `userErrors` list containing at least one error.
+    /// </summary>
+    public GraphRequestUserErrorHandling UserErrorHandling { get; set; } = GraphRequestUserErrorHandling.Throw;
 }
 
