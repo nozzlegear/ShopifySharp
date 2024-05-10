dbca0b5aa264a4bc1f13ef2365e5fb8132318d8f
Joshua Harms <joshua@nozzlegear.com>
2024-05-09 22:34:44 -0500

    fix: Make LeakyBucketRequest disposable and dispose its SemaphoreSlim

--- ShopifySharp/Infrastructure/Policies/LeakyBucketPolicy/LeakyBucketRequest.cs
+++ ShopifySharp/Infrastructure/Policies/LeakyBucketPolicy/LeakyBucketRequest.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Threading;
 
 namespace ShopifySharp.Infrastructure.Policies.LeakyBucketPolicy;
 
-internal sealed class LeakyBucketRequest(int cost, CancellationToken cancellationToken)
+internal sealed class LeakyBucketRequest(int cost, CancellationToken cancellationToken) : IDisposable
 {
     public readonly int Cost = cost;
     public readonly SemaphoreSlim Semaphore = new(0, 1);
     public readonly CancellationToken CancellationToken = cancellationToken;
+
+    public void Dispose()
+    {
+        Semaphore?.Dispose();
+    }
 }
