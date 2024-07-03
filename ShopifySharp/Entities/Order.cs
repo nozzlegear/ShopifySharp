9a575dd4b8b377cd99259a50091e1c224398c34c
Joshua Harms <joshua@nozzlegear.com>
2024-07-03 00:09:38 -0500

    refactor: remove obsolete Order.ProcessingMethod property
    BREAKING CHANGE: anyone relying on this property will need to find an alternative.

--- ShopifySharp/Entities/Order.cs
+++ ShopifySharp/Entities/Order.cs
@@ -231,13 +231,6 @@ public class Order : ShopifyObject
     public DateTimeOffset? ProcessedAt { get; set; }
 
     /// <summary>
-    /// The type of payment processing method. Known values are 'checkout', 'direct', 'manual', 'offsite', 'express', 'free' and 'none'.
-    /// </summary>
-    [JsonProperty("processing_method")]
-    [Obsolete("Deprecated in version 2023-04. https://shopify.dev/docs/api/release-notes/2023-04#payment-properties-on-the-order-resource-have-been-removed")]
-    public string ProcessingMethod { get; set; }
-
-    /// <summary>
     /// The website that the customer clicked on to come to the shop.
     /// </summary>
     [JsonProperty("referring_site")]
