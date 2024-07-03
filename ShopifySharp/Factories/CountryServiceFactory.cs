1ab4cf0c768b70c485f96af99ac6b6009821ea43
Joshua Harms <joshua@nozzlegear.com>
2024-07-03 17:04:01 -0500

    refactor: Change deprecation message for CountryService and add deprecation to interface/factory

--- ShopifySharp/Factories/CountryServiceFactory.cs
+++ ShopifySharp/Factories/CountryServiceFactory.cs
@@ -2,13 +2,16 @@
 // Notice:
 // This class is auto-generated from a template. Please do not edit it or change it directly.
 
+using System;
 using ShopifySharp.Credentials;
 using ShopifySharp.Utilities;
 
 namespace ShopifySharp.Factories;
 
+[Obsolete("https://shopify.dev/changelog/deprecation-notice-country-and-province-endpoints-in-admin-rest-api")]
 public interface ICountryServiceFactory : IServiceFactory<ICountryService>;
 
+[Obsolete("https://shopify.dev/changelog/deprecation-notice-country-and-province-endpoints-in-admin-rest-api")]
 public class CountryServiceFactory(IRequestExecutionPolicy? requestExecutionPolicy = null, IShopifyDomainUtility? shopifyDomainUtility = null) : ICountryServiceFactory
 {
     /// <inheritDoc />
