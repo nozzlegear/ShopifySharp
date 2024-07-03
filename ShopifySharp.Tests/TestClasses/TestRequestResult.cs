28f3bd3384add7e41bd6b162100bae2e2c7cc1a5
Joshua Harms <joshua@nozzlegear.com>
2024-07-02 23:42:49 -0500

    refactor: remove obsolete RequestResult.Response property
    BREAKING CHANGE: this property was marked obsolete due to a potential memory leak bug several months ago. However, anyone with a custom request execution policy may have been using it for response logging or classification.

--- ShopifySharp.Tests/TestClasses/TestRequestResult.cs
+++ ShopifySharp.Tests/TestClasses/TestRequestResult.cs
@@ -5,18 +5,9 @@ using System.Net.Http;
 
 namespace ShopifySharp.Tests.TestClasses;
 
-public class TestRequestResult<T> : RequestResult<T>
-{
-    public TestRequestResult(
-        T result
-    ) : base("",
-        new HttpResponseMessage(HttpStatusCode.OK),
-        new HttpResponseMessage(HttpStatusCode.OK).Headers,
-        result,
-        "",
-        "",
-        HttpStatusCode.OK
-    )
-    {
-    }
-}
+public class TestRequestResult<T>(T result) : RequestResult<T>("",
+    new HttpResponseMessage(HttpStatusCode.OK).Headers,
+    result,
+    "",
+    "",
+    HttpStatusCode.OK);
