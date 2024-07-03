2ee801d7a22a3c1af4a10eb26a6c10dfdfe63606
Joshua Harms <joshua@nozzlegear.com>
2024-07-02 23:26:33 -0500

    refactor: move ShopifyHttpException into ShopifySharp namespace
    BREAKING CHANGE: This is a breaking change, as the exception was previously in the `ShopifySharp.Infrastructure` namespace. It was the only exception in that namespace, and although I'd prefer all exceptions be in their own namespace instead of the shared ShopifySharp namespace, that one in particular isn't great.

--- ShopifySharp/Infrastructure/ShopifyHttpException.cs
+++ ShopifySharp/Infrastructure/ShopifyHttpException.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 
-namespace ShopifySharp.Infrastructure;
+namespace ShopifySharp;
 
 public class ShopifyHttpException(
     HttpStatusCode statusCode,
