99656ae3b179f42de51aee10968099acc84fb7c0
Joshua Harms <joshua@nozzlegear.com>
2024-10-03 01:11:07 -0500

    Fix: add missing #if NET60_OR_GREATER check on `required` keyword
    Type: fix

--- ShopifySharp/Services/Graph/GraphExtensions.cs
+++ ShopifySharp/Services/Graph/GraphExtensions.cs
@@ -1,8 +1,15 @@
 #nullable enable
 
+using System;
+
 namespace ShopifySharp.Graph;
 
+[Serializable]
 public class GraphExtensions
 {
+    #if NET60_OR_GREATER
     public required GraphRequestCostExtension Cost { get; set; }
+    #else
+    public GraphRequestCostExtension Cost { get; set; }
+    #endif
 }
