1afca3132b9edd1858fe223f929788310844e67c
Joshua Harms <joshua@nozzlegear.com>
2024-10-20 23:35:52 -0500

    Throw a ShopifyGraphUserErrorsException when user errors are detected

--- ShopifySharp/Services/Graph/GraphUserError.cs
+++ ShopifySharp/Services/Graph/GraphUserError.cs
@@ -1,20 +1,22 @@
 #nullable enable
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
-using Newtonsoft.Json;
 
 namespace ShopifySharp.Graph;
 
 [Serializable]
-public class GraphUserError
+public record GraphUserError
 {
     /// The path to the input field that caused the error.
     [JsonPropertyName("field")]
-    public IEnumerable<string> Field { get; set; } = [];
+    public ICollection<string> Field { get; set; } = [];
+
     /// The error message.
     [JsonPropertyName("message")]
     public string Message { get; set; } = string.Empty;
+
     [JsonPropertyName("code")]
     public string? Code { get; set; }
 }
