ffcf82419173d3f78c3267472f8b04095ce22244
Joshua Harms <joshua@nozzlegear.com>
2024-10-20 22:37:21 -0500

    Add ShopifyGraphUserErrorsException

---
+++ ShopifySharp/Infrastructure/ShopifyGraphUserErrorsException.cs
@@ -0,0 +1,36 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShopifySharp.Graph;
+
+namespace ShopifySharp;
+
+[Serializable]
+public class ShopifyGraphUserErrorsException(
+    ICollection<GraphUserError> graphUserErrors,
+    string? requestId = null,
+    // GraphRequestCostExtension? requestCost = null,
+    ShopifyException? innerException = null)
+    : ShopifyException(FormatMessage(graphUserErrors), innerException)
+{
+    public ICollection<GraphUserError> GraphUserErrors { get; } = graphUserErrors;
+
+    public string? RequestId { get; } = requestId;
+
+    // TODO: add support for parsing request cost and passing to exception
+    // public GraphRequestCostExtension? GraphCost { get; } = graphCost;
+
+    private static string FormatMessage(ICollection<GraphUserError> graphUserErrors)
+    {
+        var firstError = graphUserErrors.FirstOrDefault();
+
+        if (!string.IsNullOrWhiteSpace(firstError?.Code))
+            return $"{firstError.Code}: {firstError.Message}";
+
+        if (!string.IsNullOrWhiteSpace(firstError?.Message))
+            return firstError.Message;
+
+        return "GraphQL operation returned one or more errors.";
+    }
+}
