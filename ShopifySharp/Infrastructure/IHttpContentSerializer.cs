aba71030e6ab931722c05edb479a0ed1d07da423
Joshua Harms <joshua@nozzlegear.com>
2024-11-11 01:00:28 -0600

    Implement GraphHttpContentSerializer

--- ShopifySharp/Infrastructure/IHttpContentSerializer.cs
+++ ShopifySharp/Infrastructure/IHttpContentSerializer.cs
@@ -1,12 +1,10 @@
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ShopifySharp.Infrastructure;
 
 public interface IHttpContentSerializer
 {
-    // public bool CanSerializeRequest(RequestType requestType, RequestUri requestUri);
-    // public HttpContent SerializeRequest(RequestType requestType, RequestUri requestUri, IReadOnlyDictionary<string, object> requestData);
     public HttpContent SerializeGraphRequest(RequestUri requestUri, GraphRequest graphRequest);
-    // public HttpContent SerializeRestRequest(RequestUri requestUri, RestRequest restRequest);
-    // public HttpContent SerializePartnerRequest(RequestUri requestUri, PartnerRequest partnerRequest);
 }
