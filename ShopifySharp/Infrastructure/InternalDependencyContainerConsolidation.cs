6ddb287e98b200d8f0bec4c7558d9cce8af36bdc
Joshua Harms <joshua@nozzlegear.com>
2024-11-23 23:16:21 -0600

    Add tests for GraphService constructors

--- ShopifySharp/Infrastructure/InternalDependencyContainerConsolidation.cs
+++ ShopifySharp/Infrastructure/InternalDependencyContainerConsolidation.cs
@@ -6,30 +6,9 @@ namespace ShopifySharp.Infrastructure;
 
 internal static class InternalDependencyContainerConsolidation
 {
-    public static TService GetServiceOrDefault<TService, TDefault>(IDependencyContainer? container)
-        where TDefault : TService
+    public static TService GetServiceOrDefault<TService>(IDependencyContainer? container, Func<TService> createDefaultService)
     {
         var service = container != null ? container.TryGetService<TService>() : default;
-
-        if (service is not null)
-            return service;
-
-        // If the service isn't available in the container, try to resolve dependencies for TDefault
-        var constructor = typeof(TDefault).GetConstructors().FirstOrDefault();
-        if (constructor == null)
-            throw new InvalidOperationException($"No public constructor found for type {typeof(TDefault)}.");
-
-        var parameters = constructor.GetParameters()
-            .Select(p => container?.TryGetService(p.ParameterType) ?? throw new InvalidOperationException($"Unable to resolve dependency for {p.ParameterType} required by {typeof(TDefault)}"))
-            .ToArray();
-
-        // Create an instance of TDefault using resolved parameters
-        return (TService)constructor.Invoke(parameters);
-    }
-
-    public static TService GetServiceOrDefault<TService>(IDependencyContainer? container, TService @default)
-    {
-        var service = container != null ? container.TryGetService<TService>() : default;
-        return service ?? @default;
+        return service ?? createDefaultService.Invoke();
     }
 }
