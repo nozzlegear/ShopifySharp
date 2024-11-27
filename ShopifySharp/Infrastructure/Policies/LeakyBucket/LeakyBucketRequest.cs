78760ff381e34595e24149a42a0d1330d75df39a
Clement <clement.gutel@gmail.com>
2024-11-27 17:54:17 +1100

    Removed code that removes item upon TaskCanceledException. It is not necessary as the request will be simply dequeued when TryGrantNextPendingRequest gets called shortly after.

--- ShopifySharp/Infrastructure/Policies/LeakyBucket/LeakyBucketRequest.cs
+++ ShopifySharp/Infrastructure/Policies/LeakyBucket/LeakyBucketRequest.cs
@@ -1,16 +1,36 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ShopifySharp.Infrastructure.Policies.LeakyBucket;
 
 internal sealed class LeakyBucketRequest(int cost, CancellationToken cancellationToken) : IDisposable
 {
     public readonly int Cost = cost;
-    public readonly SemaphoreSlim Semaphore = new(0, 1);
+
     public readonly CancellationToken CancellationToken = cancellationToken;
+
+    public bool IsDisposed { get; private set; }
+
+    private readonly SemaphoreSlim Semaphore = new(0, 1);
 
     public void Dispose()
     {
-        Semaphore?.Dispose();
+        if (IsDisposed)
+            return;
+
+        Semaphore.Dispose();
+        IsDisposed = true;
+    }
+
+    public void Release()
+    {
+        if (!IsDisposed)
+            Semaphore.Release();
+    }
+
+    public Task WaitAsync(CancellationToken t)
+    {
+        return Semaphore.WaitAsync(t);
     }
 }
