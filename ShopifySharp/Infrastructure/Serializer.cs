5687b95b0e7d031cf60cec41b116827227fda037
Joshua Harms <joshua@nozzlegear.com>
2024-12-02 16:24:44 -0600

    Set GraphSerializerOptions' PropertyNamingPolicy to CamelCase

--- ShopifySharp/Infrastructure/Serializer.cs
+++ ShopifySharp/Infrastructure/Serializer.cs
@@ -36,6 +36,7 @@ public static class Serializer
         {
             new JsonStringEnumConverter()
         },
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
     };
 
