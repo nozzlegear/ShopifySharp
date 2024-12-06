9dcf9c7e6b125882ac1121efd60711ce77b7b22c
Clement <clement.gutel@gmail.com>
2024-12-06 16:33:24 +1100

    Regenerated GraphQL entities with GraphQLSchemaGenerator v1.7 Incremented version to 6.22

--- ShopifySharp/Entities/GraphQL/GraphQLSchema.generated.cs
+++ ShopifySharp/Entities/GraphQL/GraphQLSchema.generated.cs
@@ -67,38 +67,54 @@ namespace ShopifySharp.GraphQL
     public interface IConnection
     {
         PageInfo? pageInfo { get; set; }
+
+        Type GetNodeType();
+        IEnumerable? GetNodes();
     }
 
     public interface IConnectionWithNodes : IConnection
     {
         IEnumerable? nodes { get; set; }
+
+        IEnumerable? IConnection.GetNodes() => this.nodes;
     }
 
     public interface IConnectionWithNodes<TNode> : IConnectionWithNodes
     {
         IEnumerable? IConnectionWithNodes.nodes { get => this.nodes; set => this.nodes = (IEnumerable<TNode>? )value; }
         new IEnumerable<TNode>? nodes { get; set; }
+
+        Type IConnection.GetNodeType() => typeof(TNode);
     }
 
     public interface IConnectionWithEdges : IConnection
     {
         IEnumerable<IEdge>? edges { get; set; }
+
+        Type GetEdgeType();
+        IEnumerable? IConnection.GetNodes() => this.edges?.Select(e => e.node);
     }
 
     public interface IConnectionWithEdges<TNode> : IConnectionWithEdges
     {
         IEnumerable<IEdge>? IConnectionWithEdges.edges { get => this.edges; set => this.edges = (IEnumerable<IEdge<TNode>>? )value; }
         new IEnumerable<IEdge<TNode>>? edges { get; set; }
+
+        Type IConnection.GetNodeType() => typeof(TNode);
     }
 
     public interface IConnectionWithEdges<TEdge, TNode> : IConnectionWithEdges<TNode> where TEdge : IEdge<TNode>
     {
         IEnumerable<IEdge<TNode>>? IConnectionWithEdges<TNode>.edges { get => this.edges?.Cast<IEdge<TNode>>(); set => this.edges = value?.Cast<TEdge>(); }
         new IEnumerable<TEdge>? edges { get; set; }
+
+        Type IConnectionWithEdges.GetEdgeType() => typeof(TEdge);
     }
 
     public interface IConnectionWithNodesAndEdges<TEdge, TNode> : IConnectionWithEdges<TEdge, TNode>, IConnectionWithNodes<TNode> where TEdge : IEdge<TNode>
     {
+        Type IConnection.GetNodeType() => typeof(TNode);
+        IEnumerable? IConnection.GetNodes() => this.nodes ?? this.edges?.Select(e => e.node);
     }
 
     ///<summary>
