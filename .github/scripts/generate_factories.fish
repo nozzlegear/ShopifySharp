cb4dc6125485d60441842cb30b7965cfa1f80b5d
Joshua Harms <joshua@nozzlegear.com>
2024-12-12 01:46:51 -0600

    Replace custom fish factory-service-template.txt with a T4 template

--- .github/scripts/generate_factories.fish
+++ .github/scripts/generate_factories.fish
@@ -1,6 +1,6 @@
 #! /usr/bin/env fish
 
-set template_path "ShopifySharp/Factories/factory-service-template.txt"
+set template_path "ShopifySharp/Factories/FactoryServiceTemplate.tt"
 
 if ! test -e "$template_path"
     set_color yellow
@@ -11,13 +11,7 @@ end
 for service_file in (ls ShopifySharp/Services/*/I*Service.cs)
     set service_name (rg -o -e "public interface I([A-Z]\w+)Service" "$service_file")
     set service_name (string replace "public interface I" "" "$service_name")
-
-    # The PartnerService has a different type of constructor
-    if test -n "$service_name" -a "$service_name" != "PartnerService"
-        set factory_file_name (printf "ShopifySharp/Factories/%sFactory.cs" "$service_name")
-        set templated_service_text $template_text
-        echo $service_name
-
-        string replace --all "@@REPLACEME@@" "$service_name" (cat "$template_path") > "$factory_file_name"
-    end
+    echo $service_name
+    set factory_file_name (printf "ShopifySharp/Factories/%sFactory.cs" "$service_name")
+    dotnet t4 -p "TypeName=$service_name" -o "$factory_file_name" "$template_path"
 end
--- ShopifySharp.Tests/Factories/PartnerServiceFactoryTest.cs
+++ ShopifySharp.Tests/Factories/PartnerServiceFactoryTest.cs
@@ -1,8 +1,10 @@
 #nullable enable
+using FakeItEasy;
 using FluentAssertions;
 using JetBrains.Annotations;
 using ShopifySharp.Credentials;
 using ShopifySharp.Factories;
+using ShopifySharp.Infrastructure;
 using Xunit;
 
 namespace ShopifySharp.Tests.Factories;
@@ -14,13 +16,19 @@ public class PartnerServiceFactoryTest
     private const long OrganizationId = 123L;
     private const string AccessToken = "some-access-token";
 
-    private readonly PartnerServiceFactory _factory = new();
+    private readonly IDependencyContainer _dependencyContainer = A.Fake<IDependencyContainer>();
+    private readonly PartnerServiceFactory _sut;
+
+    public PartnerServiceFactoryTest()
+    {
+        _sut = new PartnerServiceFactory(_dependencyContainer);
+    }
 
     [Fact]
     public void Create_ReturnsTheService()
     {
         // Act
-        var service = _factory.Create(OrganizationId, AccessToken);
+        var service = _sut.Create(OrganizationId, AccessToken);
 
         // Assert
         service.Should().NotBeNull();
@@ -34,7 +42,7 @@ public class PartnerServiceFactoryTest
         var credentials = new ShopifyPartnerApiCredentials(OrganizationId, AccessToken);
 
         // Act
-        var service = _factory.Create(credentials);
+        var service = _sut.Create(credentials);
 
         // Assert
         service.Should().NotBeNull();
