7b503eaf4897ce5c915563521a557c3dadfbab98
Joshua Harms <joshua@nozzlegear.com>
2024-12-17 18:47:14 -0600

    WIP refactoring factory constructors and fixing DI package tests

--- ShopifySharp.Extensions.DependencyInjection.Tests/FactoryServiceTests.cs
+++ ShopifySharp.Extensions.DependencyInjection.Tests/FactoryServiceTests.cs
@@ -1,8 +1,42 @@
+using FakeItEasy;
+using ShopifySharp.Utilities;
+
 namespace ShopifySharp.Extensions.DependencyInjection.Tests;
 
 public class FactoryServiceTests
 {
     private readonly ShopifyApiCredentials _credentials = new("some-shopify-domain", "some-access-token");
+
+    [Fact]
+    public void FactoryServices_WhenConstructingService_ShouldUseServiceProviderConstructor()
+    {
+        // Setup
+        var sut = A.Fake<IServiceProvider>(x => x.Strict());
+        var factory = new OrderServiceFactory(sut);
+        var getDomainServiceCall = A.CallTo(() => sut.GetService(typeof(IShopifyDomainUtility)));
+        var getExecutionPolicyCall = A.CallTo(() => sut.GetService(typeof(IRequestExecutionPolicy)));
+
+        getDomainServiceCall.Returns(A.Dummy<IShopifyDomainUtility>());
+        getExecutionPolicyCall.Returns(A.Dummy<IRequestExecutionPolicy>());
+
+        // Act
+        var act = () => factory.Create(_credentials);
+
+        // Assert
+        act.Should()
+            .NotThrow()
+            .Subject
+            .Should()
+            .BeOfType<OrderService>();
+        getDomainServiceCall.MustHaveHappened();
+        getExecutionPolicyCall.MustHaveHappened();
+    }
+
+    [Fact]
+    public void FactoryServices_WhenConstructingFactoryViaObsoleteConstructor_ShouldPassServicesToInternalServiceProvider()
+    {
+
+    }
 
     [Fact]
     public void FactoryServices_WhenRequestExecutionPolicyIsNotInjected_ShouldCreateService()
@@ -57,7 +91,7 @@ public class FactoryServiceTests
 
         // Act
         var orderServiceFactory = serviceProvider.GetRequiredService<IOrderServiceFactory>();
-        // The constructor will use the domain utility to turn the domain string into a uri
+        // The constructor will use the domain utility to turn the domain string into an uri
         var act = () => orderServiceFactory.Create(_credentials);
 
         // Assert
