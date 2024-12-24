7936873ca2bddb1788ef948625c4f52602da6a09
Joshua Harms <joshua@nozzlegear.com>
2024-12-23 23:25:47 -0600

    Refactor exception constructors to accept Exception as the inner exception type

--- ShopifySharp/Infrastructure/ShopifyJsonParseException.cs
+++ ShopifySharp/Infrastructure/ShopifyJsonParseException.cs
@@ -7,15 +7,18 @@ namespace ShopifySharp;
 [Serializable]
 public class ShopifyJsonParseException(
     string message,
-    string? jsonPropertyName,
+    string? jsonPropertyName = null,
     string? requestId = null,
-    ShopifyException? innerException = null)
+    Exception? innerException = null)
     : ShopifyException(message, innerException)
 {
     /// <summary>
-    /// The name of the json property that caused the exception. May be null if it was not possible to capture the name.
+    /// The name of the JSON property that caused the exception. May be null if it was not possible to capture the name.
     /// </summary>
     public string? JsonPropertyName { get; } = jsonPropertyName;
 
+    /// <summary>
+    /// The request ID associated with the exception, if available.
+    /// </summary>
     public string? RequestId { get; } = requestId;
 }
