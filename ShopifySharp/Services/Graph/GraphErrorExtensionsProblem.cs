0e019b23a6db5b99f30cbe55f3711e01f3f9cb32
Joshua Harms <joshua@nozzlegear.com>
2025-02-04 20:12:33 -0600

    converters: add PrimitiveStringConverter and PrimitiveStringListConverter to ints/bools to strings
    fixes #1152

--- ShopifySharp/Services/Graph/GraphErrorExtensionsProblem.cs
+++ ShopifySharp/Services/Graph/GraphErrorExtensionsProblem.cs
@@ -1,10 +1,13 @@
 #nullable enable
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using ShopifySharp.Converters.SystemTextJson;
 
 namespace ShopifySharp.Services.Graph;
 
 public record GraphErrorExtensionsProblem
 {
+    [JsonConverter(typeof(PrimitiveStringConverterFactory))]
     public IReadOnlyList<string>? Path { get; set; }
 
     public string? Explanation { get; set; }
