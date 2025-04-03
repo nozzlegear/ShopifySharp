396be64ce3457f510aa813f449a202c43f85e2e2
Joshua Harms <joshua@nozzlegear.com>
2025-04-03 14:22:27 -0500

    Add additional tests for ShopifyOauthUtility.RefreshAccessTokenAsync
    Scope: tests

--- ShopifySharp.Tests/Utilities/ShopifyOauthUtilityTests.cs
+++ ShopifySharp.Tests/Utilities/ShopifyOauthUtilityTests.cs
@@ -225,38 +225,6 @@ public class ShopifyOauthUtilityTests
         act.Should().Throw<ArgumentException>()
             .WithMessage("Invalid AuthorizationUrlOptions. Cannot use the obsolete Grants alongside AuthorizationAccessMode.");
     }
-    #endif
-
-    #if NET8_0_OR_GREATER
-    [Fact]
-    public async Task RefreshAccessTokenAsync_ShouldNotDisposeHttpClient()
-    {
-        // This is testing the fix for the issue described in #1005 and #1006
-
-        // Setup
-        const string shopDomain = "some-shop-domain";
-        var factory = new InternalHttpClientFactory();
-        var client = factory.CreateClient("some-name");
-
-        // Act
-        var refresh = async () => await _sut.RefreshAccessTokenAsync(new RefreshAccessTokenOptions
-        {
-            ClientId = "some-client-id",
-            ClientSecret = "some-client-secret",
-            ExistingStoreAccessToken = "some-existing-store-access-token",
-            RefreshToken = "some-refresh-token",
-            ShopDomain = shopDomain
-        });
-        var send = async () =>
-        {
-            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, shopDomain));
-            response.EnsureSuccessStatusCode();
-        };
-
-        // Assert
-        await refresh.Should().ThrowAsync<HttpRequestException>();
-        await send.Should().NotThrowAsync<ObjectDisposedException>();
-    }
 
     [Fact(DisplayName = "AuthorizeAsync(AuthorizeOptions) should call the base AuthorizeAsync(string, string, string, string) method")]
     public async Task AuthorizeAsync_WithAuthorizeOptionsParameters_ShouldCallBaseAuthorizeAsyncMethodAndPassOptionsToMethod()
@@ -282,8 +250,7 @@ public class ShopifyOauthUtilityTests
         await act.Should().ThrowAsync<TestException>();
         callToDomainUtil.MustHaveHappenedOnceExactly();
     }
-
-#endif
+    #endif
 
     [Fact]
     public async Task AuthorizeAsync_WhenAnErrorIsReturned_ShouldThrow()
@@ -587,6 +554,309 @@ public class ShopifyOauthUtilityTests
         else
             onlineAccess.AssociatedUserScopes.Should().BeEquivalentTo(scopes);
     }
+
+    #region RefreshAccessTokenAsync tests
+
+    [Fact]
+    public async Task RefreshAccessTokenAsync_WhenAnErrorIsReturned_ShouldThrow()
+    {
+        // Setup
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.BadRequest;
+        const string expectedErrorMessage = "some-error-message";
+        const string json = $$"""{ "error": "{{expectedErrorMessage}}" }""";
+        var result = Utils.MakeHttpResponseMessage(json, x => x.StatusCode = expectedStatusCode);
+
+        A.CallTo(() => _httpClient.SendAsync(A<HttpRequestMessage>._, CancellationToken.None))
+            .Returns(result);
+
+        // Act
+        var act = async () => await _sut.RefreshAccessTokenAsync(
+            ShopDomain,
+            ClientId,
+            "some-client-secret",
+            "some-refresh-token",
+            "some-existing-store-access-token"
+        );
+
+        // Assert
+        var exn = await act.Should().ThrowAsync<ShopifyHttpException>()
+            .WithMessage("(400 Bad Request) " + expectedErrorMessage);
+        exn.Which.HttpStatusCode.Should().Be(expectedStatusCode);
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public async Task RefreshAccessTokenAsync_WhenNoAssociatedUserIsPresent_ShouldPass(bool includeNullValue)
+    {
+        // Setup
+        const string accessToken = "some-access-token";
+        var json = includeNullValue
+            //lang=json
+            ? $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": "",
+                "user": null
+              }
+              """
+            //lang=json
+            : $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": ""
+              }
+              """;
+        var result = Utils.MakeHttpResponseMessage(json);
+
+        A.CallTo(() => _httpClient.SendAsync(A<HttpRequestMessage>._, CancellationToken.None))
+            .Returns(result);
+
+        // Act
+        var authorizationResult = await _sut.RefreshAccessTokenAsync(
+            ShopDomain,
+            ClientId,
+            "some-client-secret",
+            "some-refresh-token",
+            "some-existing-store-access-token"
+        );
+
+        // Assert
+        authorizationResult.AccessToken.Should().Be(accessToken);
+        authorizationResult.GrantedScopes.Should().BeEmpty();
+        authorizationResult.OnlineAccess.Should().BeNull();
+        authorizationResult.IsOnlineAccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task RefreshAccessTokenAsync_WhenAnAssociatedUserIsPresent_ShouldPassAndReturnAnAuthorizationResultWithAnAssociatedUser()
+    {
+        // Setup
+        const int expiresIn = 123;
+        const string accessToken = "some-access-token";
+        var expectedUser = new AssociatedUser
+        {
+            Id = 7,
+            FirstName = "some-first-name",
+            LastName = "some-last-name",
+            Email = "some-email",
+            EmailVerified = true,
+            AccountOwner = true,
+            Locale = "some-locale",
+            Collaborator = true
+        };
+        var json =
+            //lang=json
+            $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": "",
+                "associated_user_scope": "",
+                "expires_in": {{expiresIn}},
+                "associated_user": {
+                  "id": {{expectedUser.Id}},
+                  "first_name": "{{expectedUser.FirstName}}",
+                  "last_name": "{{expectedUser.LastName}}",
+                  "email": "{{expectedUser.Email}}",
+                  "email_verified": {{expectedUser.EmailVerified.ToString().ToLower()}},
+                  "account_owner": {{expectedUser.AccountOwner.ToString().ToLower()}},
+                  "locale": "{{expectedUser.Locale}}",
+                  "collaborator": {{expectedUser.Collaborator.ToString().ToLower()}}
+                }
+              }
+              """;
+        var result = Utils.MakeHttpResponseMessage(json);
+
+        A.CallTo(() => _httpClient.SendAsync(A<HttpRequestMessage>._, CancellationToken.None))
+            .Returns(result);
+
+        // Act
+        var authorizationResult = await _sut.RefreshAccessTokenAsync(
+            ShopDomain,
+            ClientId,
+            "some-client-secret",
+            "some-refresh-token",
+            "some-existing-store-access-token"
+        );
+
+        // Assert
+        authorizationResult.IsOnlineAccess.Should().BeTrue();
+        authorizationResult.OnlineAccess.Should().NotBeNull();
+
+        var onlineAccess = authorizationResult.OnlineAccess!;
+        onlineAccess.ExpiresIn.Should().BeCloseTo(TimeSpan.FromSeconds(expiresIn), TimeSpan.FromSeconds(3), "expiration should be within 3 seconds of test value");
+        onlineAccess.AssociatedUserScopes.Should().BeEmpty();
+        onlineAccess.AssociatedUser.Should().BeEquivalentTo(expectedUser);
+    }
+
+    [Theory]
+    [InlineData(true, null)]
+    [InlineData(true, "")]
+    [InlineData(true, " ", "", "    ")]
+    [InlineData(true, " ", "", " ", "   ", "    ")]
+    [InlineData(false, "granted_scope_1", "granted_scope_2", "granted_scope_3")]
+    public async Task RefreshAccessTokenAsync_ShouldParseScopesAndPass(bool expectEmpty, params string[]? scopes)
+    {
+        // Setup
+        const string accessToken = "some-access-token";
+        var json = scopes is null ?
+            //lang=json
+            $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": ""
+              }
+              """
+            // lang=json
+            : $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": "{{string.Join(",", scopes)}}"
+              }
+              """;
+        var result = Utils.MakeHttpResponseMessage(json);
+
+        var callToClient = A.CallTo(() => _httpClient.SendAsync(A<HttpRequestMessage>._, CancellationToken.None));
+        callToClient.Returns(result);
+
+        // Act
+        var authorizationResult = await _sut.RefreshAccessTokenAsync(
+            ShopDomain,
+            ClientId,
+            "some-client-secret",
+            "some-refresh-token",
+            "some-existing-store-access-token"
+        );
+
+        // Assert
+        authorizationResult.AccessToken.Should().Be(accessToken);
+        authorizationResult.OnlineAccess.Should().BeNull();
+        authorizationResult.IsOnlineAccess.Should().BeFalse();
+
+        if (expectEmpty)
+            authorizationResult.GrantedScopes.Should().BeEmpty();
+        else
+            authorizationResult.GrantedScopes.Should().BeEquivalentTo(scopes);
+    }
+
+    [Theory]
+    [InlineData(true, null)]
+    [InlineData(true, "")]
+    [InlineData(true, " ", "", "    ")]
+    [InlineData(true, " ", "", " ", "   ", "    ")]
+    [InlineData(false, "granted_scope_1", "granted_scope_2", "granted_scope_3")]
+    public async Task RefreshAccessTokenAsync_WhenAnAssociatedUserIsPresent_ShouldParseAssociatedUserScopeAndPass(bool expectEmpty, params string[]? scopes)
+    {
+        // Setup
+        const int expiresIn = 123;
+        const string accessToken = "some-access-token";
+        var expectedUser = new AssociatedUser
+        {
+            Id = 7,
+            FirstName = "some-first-name",
+            LastName = "some-last-name",
+            Email = "some-email",
+            EmailVerified = true,
+            AccountOwner = true,
+            Locale = "some-locale",
+            Collaborator = true
+        };
+        var associatedUserJson =
+            //lang=json
+            $$"""
+              { 
+                "id": {{expectedUser.Id}},
+                "first_name": "{{expectedUser.FirstName}}",
+                "last_name": "{{expectedUser.LastName}}",
+                "email": "{{expectedUser.Email}}",
+                "email_verified": {{expectedUser.EmailVerified.ToString().ToLower()}},
+                "account_owner": {{expectedUser.AccountOwner.ToString().ToLower()}},
+                "locale": "{{expectedUser.Locale}}",
+                "collaborator": {{expectedUser.Collaborator.ToString().ToLower()}}
+              }
+              """;
+        var json = scopes is null ?
+            //lang=json
+            $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": "",
+                "expires_in": {{expiresIn}},
+                "associated_user": {{associatedUserJson}}
+              }
+              """
+            // lang=json
+            : $$"""
+              { 
+                "access_token": "{{accessToken}}",
+                "scope": "",
+                "expires_in": {{expiresIn}},
+                "associated_user": {{associatedUserJson}},
+                "associated_user_scope": "{{string.Join(",", scopes)}}"
+              }
+              """;
+        var result = Utils.MakeHttpResponseMessage(json);
+
+        A.CallTo(() => _httpClient.SendAsync(A<HttpRequestMessage>._, CancellationToken.None))
+            .Returns(result);
+
+        // Act
+        var authorizationResult = await _sut.RefreshAccessTokenAsync(
+            ShopDomain,
+            ClientId,
+            "some-client-secret",
+            "some-refresh-token",
+            "some-existing-store-access-token"
+        );
+
+        // Assert
+        authorizationResult.AccessToken.Should().Be(accessToken);
+        authorizationResult.GrantedScopes.Should().BeEmpty();
+        authorizationResult.OnlineAccess.Should().NotBeNull();
+        authorizationResult.IsOnlineAccess.Should().BeTrue();
+
+        var onlineAccess = authorizationResult.OnlineAccess!;
+        onlineAccess.ExpiresIn.Should().BeCloseTo(TimeSpan.FromSeconds(expiresIn), TimeSpan.FromSeconds(3), "expiration should be within 3 seconds of test value");
+        onlineAccess.AssociatedUser.Should().BeEquivalentTo(expectedUser);
+
+        if (expectEmpty)
+            onlineAccess.AssociatedUserScopes.Should().BeEmpty();
+        else
+            onlineAccess.AssociatedUserScopes.Should().BeEquivalentTo(scopes);
+    }
+
+    #if NET8_0_OR_GREATER
+    [Fact]
+    public async Task RefreshAccessTokenAsync_ShouldNotDisposeHttpClient()
+    {
+        // This is testing the fix for the issue described in #1005 and #1006
+
+        // Setup
+        const string shopDomain = "some-shop-domain";
+        var factory = new InternalHttpClientFactory();
+        var client = factory.CreateClient("some-name");
+
+        // Act
+        var refresh = async () => await _sut.RefreshAccessTokenAsync(new RefreshAccessTokenOptions
+        {
+            ClientId = "some-client-id",
+            ClientSecret = "some-client-secret",
+            ExistingStoreAccessToken = "some-existing-store-access-token",
+            RefreshToken = "some-refresh-token",
+            ShopDomain = shopDomain
+        });
+        var send = async () =>
+        {
+            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, shopDomain));
+            response.EnsureSuccessStatusCode();
+        };
+
+        // Assert
+        await refresh.Should().ThrowAsync<HttpRequestException>();
+        await send.Should().NotThrowAsync<ObjectDisposedException>();
+    }
+    #endif
+
+    #endregion
 
     #region AuthorizeAsync – Null and invalid type tests
 
