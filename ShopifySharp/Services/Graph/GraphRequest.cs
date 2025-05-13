81ab272a645683d1dcff6feaf7e368f4e4d990e4
Joshua Harms <joshua@nozzlegear.com>
2025-05-13 00:24:22 -0500

    Remove obsolete GraphRequest `.query` and `.variables` backing fields
    Made obsolete in c231203d
    
    Scope: graphql, infrastructure

--- ShopifySharp/Services/Graph/GraphRequest.cs
+++ ShopifySharp/Services/Graph/GraphRequest.cs
@@ -1,7 +1,5 @@
 #nullable enable
-using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using ShopifySharp.Services.Graph;
 
 namespace ShopifySharp;
@@ -9,23 +7,12 @@ namespace ShopifySharp;
 // TODO: move this to ShopifySharp.Services.Graph namespace
 public class GraphRequest
 {
-    private string? _query { get; set; }
-    private IDictionary<string, object>? _variables { get; set; }
-
     #if NET8_0_OR_GREATER
-    [StringSyntax("graphql")]
+    [System.Diagnostics.CodeAnalysis.StringSyntax("graphql")]
     #endif
-    public string Query
-    {
-        get => _query ?? "";
-        set => _query = value;
-    }
+    public string Query { get; set; } = "";
 
-    public IDictionary<string, object>? Variables
-    {
-        get => _variables;
-        set => _variables = value;
-    }
+    public IDictionary<string, object>? Variables { get; set; }
 
     /// <summary>
     /// What you estimate this query will cost. Use this value to provide an estimated query cost to any <see cref="IRequestExecutionPolicy" />, to avoid sending a request that will be throttled. This value can be found in the query response data at <c>extensions.cost.requestedQueryCost</c>.
