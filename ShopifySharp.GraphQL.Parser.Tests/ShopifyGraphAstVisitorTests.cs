ff13c1cd83b94126b509817a9cc4bf0eb4647183
Joshua Harms <joshua@nozzlegear.com>
2025-05-15 00:06:00 -0500

    WIP: implement tests for parsing enums, type definitions and interfaces

--- ShopifySharp.GraphQL.Parser.Tests/ShopifyGraphAstVisitorTests.cs
+++ ShopifySharp.GraphQL.Parser.Tests/ShopifyGraphAstVisitorTests.cs
@@ -44,69 +44,6 @@ public class ShopifyGraphAstVisitorTests
     }
 
     [Fact]
-    public async Task ShouldParseSchemaDefinitions()
-    {
-        // Setup
-        //lang=gql
-        var graphql =
-            """"
-            schema {
-              query: QueryRoot
-              mutation: Mutation
-            }
-
-            """Marks an element of a GraphQL schema as having restricted access."""
-            directive @accessRestricted(
-              """Explains the reason around this restriction"""
-              reason: String = null
-            ) on FIELD_DEFINITION | OBJECT
-
-            """
-            Requires that exactly one field must be supplied and that field must not be `null`.
-            """
-            directive @oneOf on INPUT_OBJECT
-
-            """Exposes a URL that specifies the behavior of this scalar."""
-            directive @specifiedBy(
-              """The URL that specifies the behavior of this scalar."""
-              url: String!
-            ) on SCALAR
-            """";
-
-        // Act
-        var ast = GraphQLParser.Parser.Parse(graphql);
-        await _sut.VisitAsync(ast, _writerContext);
-        await _pipe.Writer.CompleteAsync();
-
-        // Assert
-        var text = await GetTextWrittenToPipeAsync();
-        text.Should().Be(
-            """
-            would write comments 0
-            would write description [none]
-            would write directives 0
-            root operation: QueryRoot
-            root operation: Mutation
-            directive desc: Marks an element of a GraphQL schema as having restricted access.
-            directive name: accessRestricted
-            argument name: reason
-            argument desc: Explains the reason around this restriction
-            argument default value: NullValue
-            argument is child definition: True
-            directive desc: Requires that exactly one field must be supplied and that field must not be `null`.
-            directive name: oneOf
-            directive desc: Exposes a URL that specifies the behavior of this scalar.
-            directive name: specifiedBy
-            argument name: url
-            argument desc: The URL that specifies the behavior of this scalar.
-            argument default value: 
-            argument is child definition: True
-            
-            """
-        );
-    }
-    
-    [Fact]
     public async Task ShouldParseInputDefinition()
     {
         // Setup
@@ -136,19 +73,216 @@ public class ShopifyGraphAstVisitorTests
             """
             public record AppSubscriptionDiscountValueInput
             {
-            /// <summary>
-            /// The monetary value of a discount.
-            /// </summary>
-            [System.Text.Json.JsonProperty("amount")]
-            public decimal Amount { get; set; }
-            /// <summary>
-            /// The percentage value of a discount.
-            /// </summary>
-            [System.Text.Json.JsonProperty("percentage")]
-            public decimal Percentage { get; set; }
+                /// <summary>
+                /// The monetary value of a discount.
+                /// </summary>
+                [System.Text.Json.JsonProperty("amount")]
+                public decimal Amount { get; set; }
+
+                /// <summary>
+                /// The percentage value of a discount.
+                /// </summary>
+                [System.Text.Json.JsonProperty("percentage")]
+                public decimal Percentage { get; set; }
             }
 
             """
         );
+    }
+
+    [Fact]
+    public async Task ShouldParseEnumDefinition()
+    {
+        // Setup
+        const string graphql =
+            //lang=graphql
+            """"
+            """Specifies the abandonment type."""
+            enum AbandonmentAbandonmentType {
+              """The abandonment event is an abandoned browse."""
+              BROWSE
+            
+              """The abandonment event is an abandoned cart."""
+              CART
+            
+              """The abandonment event is an abandoned checkout."""
+              CHECKOUT
+            }
+            """";
+
+        // Act
+        var ast = GraphQLParser.Parser.Parse(graphql);
+        await _sut.VisitAsync(ast, _writerContext);
+        await _pipe.Writer.CompleteAsync();
+
+        // Assert
+        var text = await GetTextWrittenToPipeAsync();
+        text.Should().Be(
+            //lang=cs
+            """
+            /// <summary>
+            /// Specifies the abandonment type.
+            /// </summary>
+            public enum AbandonmentAbandonmentType
+            {
+                /// <summary>
+                /// The abandonment event is an abandoned browse.
+                /// </summary>
+                [System.Text.Json.JsonProperty("BROWSE")]
+                BROWSE,
+
+                /// <summary>
+                /// The abandonment event is an abandoned cart.
+                /// </summary>
+                [System.Text.Json.JsonProperty("CART")]
+                CART,
+
+                /// <summary>
+                /// The abandonment event is an abandoned checkout.
+                /// </summary>
+                [System.Text.Json.JsonProperty("CHECKOUT")]
+                CHECKOUT
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task ShouldParseTypeDefinition()
+    {
+        const string graphql =
+            //lang=graphql
+            """"
+            """
+            The permission required to access a Shopify Admin API or Storefront API resource
+            for a shop. Merchants grant access scopes that are requested by applications.
+            """
+            type AccessScope {
+              """
+              A description of the actions that the access scope allows an app to perform.
+              """
+              description: String!
+              """
+              A readable string that represents the access scope. The string usually follows
+              the format `{action}_{resource}`. `{action}` is `read` or `write`, and
+              `{resource}` is the resource that the action can be performed on. `{action}`
+              and `{resource}` are separated by an underscore. For example, `read_orders` or
+              `write_products`.
+              """
+              handle: String!
+            }
+            """";
+
+        // Act
+        var ast = GraphQLParser.Parser.Parse(graphql);
+        await _sut.VisitAsync(ast, _writerContext);
+        await _pipe.Writer.CompleteAsync();
+
+        // Assert
+        var text = await GetTextWrittenToPipeAsync();
+        text.Should().Be(
+            //lang=cs
+            """
+            /// <summary>
+            /// The permission required to access a Shopify Admin API or Storefront API resource
+            /// for a shop. Merchants grant access scopes that are requested by applications.
+            /// </summary>
+            public record AccessScope
+            {
+                /// <summary>
+                /// A description of the actions that the access scope allows an app to perform.
+                /// </summary>
+                [System.Text.Json.JsonProperty("description")]
+                public string Description { get; set; }
+
+                /// <summary>
+                /// A readable string that represents the access scope. The string usually follows
+                /// the format `{action}_{resource}`. `{action}` is `read` or `write`, and
+                /// `{resource}` is the resource that the action can be performed on. `{action}`
+                /// and `{resource}` are separated by an underscore. For example, `read_orders` or
+                /// `write_products`.
+                /// </summary>
+                [System.Text.Json.JsonProperty("handle")]
+                public string Handle { get; set; }
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task ShouldParseInterfaces()
+    {
+        // Setup
+        const string graphql =
+            //lang=graphql
+            """"
+            """The subject line of a comment event."""
+            interface CommentEventSubject {
+              """
+              Whether the timeline subject has a timeline comment. If true, then a timeline comment exists.
+              """
+              hasTimelineComment: Boolean!
+
+              """A globally-unique ID."""
+              id: ID!
+            }
+            """";
+        
+        // Act
+        var ast = GraphQLParser.Parser.Parse(graphql);
+        await _sut.VisitAsync(ast, _writerContext);
+        await _pipe.Writer.CompleteAsync();
+
+        // Assert
+        var text = await GetTextWrittenToPipeAsync();
+        text.Should().Be(
+            //lang=cs
+            """
+            /// <summary>
+            /// The subject line of a comment event.
+            /// </summary>
+            public interface ICommentEventSubject
+            {
+                /// <summary>
+                /// Whether the timeline subject has a timeline comment. If true, then a timeline comment exists.
+                /// </summary>
+                [System.Text.Json.JsonProperty("hasTimelineComment")]
+                public bool HasTimelineComment { get; set; }
+
+                /// <summary>
+                /// A globally-unique ID.
+                /// </summary>
+                [System.Text.Json.JsonProperty("id")]
+                public string Id { get; set; }
+            }
+
+            """
+        );
+    }
+
+    [Fact]
+    public async Task ShouldParseTypesThatHaveInterfaces()
+    {
+        Assert.Fail("test not implemented");
+    }
+
+    [Fact]
+    public async Task ShouldParseUnionTypeDefinition()
+    {
+        Assert.Fail("test not implemented");
+    }
+
+    [Fact]
+    public async Task ShouldParseTypeDefinitionsThatReferenceOtherTypes()
+    {
+        Assert.Fail("test not implemented");
+    }
+
+    [Fact]
+    public async Task ShouldParseDefinitionsThatHavePropertiesWithParameters()
+    {
+        Assert.Fail("test not implemented");
     }
 }
--- ShopifySharp.GraphQL.Parser/ShopifyGraphAstVisitor.cs
+++ ShopifySharp.GraphQL.Parser/ShopifyGraphAstVisitor.cs
@@ -29,7 +29,14 @@ public class ShopifyGraphAstVisitor: ASTVisitor<WriterContext>
         { "StorefrontID", "string" },
         { "Color", "string" },
         { "BigInt", "long" },
+        { "String", "string" },
+        { "Boolean", "bool" },
+        { "Integer", "int" },
+        { "ID", "string" },
     };
+
+    public static string MakePascalCase(GraphQLName name) =>
+        char.ToUpper(name.Value.Span[0]) + name.Value.Span[1..].ToString();
 
     public static string? MapScalarNameToTypeName(string scalarName) =>
         TypeMap.GetValueOrDefault(scalarName);
@@ -39,38 +46,27 @@ public class ShopifyGraphAstVisitor: ASTVisitor<WriterContext>
         {
             GraphQLNamedType graphQlNamedType => MapScalarNameToTypeName(graphQlNamedType.Name.StringValue) ?? graphQlNamedType.Name.StringValue,
             GraphQLListType graphQlListType => $"[{MapGraphTypeToTypeName(graphQlListType.Type)}]",
-            GraphQLNonNullType graphQlNonNullType => MapGraphTypeToTypeName(graphQlNonNullType.Type) + "!",
+            GraphQLNonNullType graphQlNonNullType => MapGraphTypeToTypeName(graphQlNonNullType.Type),
             _ => throw new ArgumentOutOfRangeException(nameof(graphType))
         };
 
-    protected override async ValueTask VisitSchemaDefinitionAsync(GraphQLSchemaDefinition schemaDefinition, WriterContext context)
-    {
-        await context.WriteLineAsync("would write comments " + (schemaDefinition.Comments?.Count ?? 0));
-        await context.WriteLineAsync("would write description " + (schemaDefinition.Description?.Value ?? "[none]"));
-        await context.WriteLineAsync("would write directives " + (schemaDefinition.Directives?.Count ?? 0));
-        await VisitAsync(schemaDefinition.OperationTypes, context).ConfigureAwait(false);
-    }
-
-    protected override async ValueTask VisitRootOperationTypeDefinitionAsync(GraphQLRootOperationTypeDefinition definition, WriterContext context)
-    {
-        await context.WriteLineAsync("root operation: " + definition.Type?.Name.StringValue);
-        await base.VisitRootOperationTypeDefinitionAsync(definition, context);
-    }
-
-    protected override ValueTask VisitDirectiveAsync(GraphQLDirective directive, WriterContext context)
-    {
-        return base.VisitDirectiveAsync(directive, context);
-    }
-
-    protected override ValueTask VisitCommentAsync(GraphQLComment comment, WriterContext context)
-    {
-        return base.VisitCommentAsync(comment, context);
-    }
+    public async Task WriteJsonPropertyAttributeAsync(GraphQLName propertyName, WriterContext context) =>
+        await context.WriteLineAsync($"[System.Text.Json.JsonProperty(\"{propertyName.StringValue}\")]");
 
     protected override async ValueTask VisitDescriptionAsync(GraphQLDescription description, WriterContext context)
     {
+        var segments = new List<string>();
+        var span = description.Value.Span;
+
+        // Must split the line before any await call, as spans cannot survive the async/yield boundary and the compiler will throw an error
+        foreach (var segment in span.Split(Environment.NewLine))
+            segments.Add(span[segment].ToString());
+
         await context.WriteLineAsync("/// <summary>");
-        await context.WriteLineAsync("/// " + description.Value);
+
+        foreach (var segment in segments)
+            await context.WriteLineAsync("/// " + segment);
+
         await context.WriteLineAsync("/// </summary>");
     }
 
@@ -90,6 +86,9 @@ public class ShopifyGraphAstVisitor: ASTVisitor<WriterContext>
 
     protected override async ValueTask VisitInputObjectTypeDefinitionAsync(GraphQLInputObjectTypeDefinition inputObjectTypeDefinition, WriterContext context)
     {
+        await VisitAsync(inputObjectTypeDefinition.Comments, context).ConfigureAwait(false);
+        await VisitAsync(inputObjectTypeDefinition.Description, context).ConfigureAwait(false);
+
         await context.WriteLineAsync($"public record {inputObjectTypeDefinition.Name}");
         await context.WriteLineAsync("{");
         context.Indent();
@@ -111,6 +110,7 @@ public class ShopifyGraphAstVisitor: ASTVisitor<WriterContext>
 
         for (var i = 0; i < inputFieldsDefinition.Count; i++)
         {
+            var isLastItem = i + 1 >= inputFieldsDefinition.Count;
             var inputField = inputFieldsDefinition[i];
 
             if (inputField.Name.Value.IsEmpty)
@@ -120,12 +120,10 @@ public class ShopifyGraphAstVisitor: ASTVisitor<WriterContext>
 
             await VisitAsync(inputField.Description, context).ConfigureAwait(false);
 
-            var pascalCasedName = char.ToUpper(inputField.Name.Value.Span[0]) + inputField.Name.Value.Span[1..].ToString();
+            await WriteJsonPropertyAttributeAsync(inputField.Name, context);
+            await context.WriteLineAsync($$"""public {{fieldType}} {{MakePascalCase(inputField.Name)}} { get; set; }""");
 
-            await context.WriteLineAsync($"[System.Text.Json.JsonProperty(\"{inputField.Name.Value}\")]");
-            await context.WriteLineAsync($$"""public {{fieldType}} {{pascalCasedName}} { get; set; }""");
-
-            if (i + 1 < inputFieldsDefinition.Count)
+            if (!isLastItem)
                 await context.WriteEmptyLineAsync();
         }
     }
@@ -141,6 +139,100 @@ public class ShopifyGraphAstVisitor: ASTVisitor<WriterContext>
 
         await VisitAsync(enumTypeDefinition.Directives, context).ConfigureAwait(false);
         await VisitAsync(enumTypeDefinition.Values, context).ConfigureAwait(false);
+
+        context.Outdent();
+        await context.WriteLineAsync("}");
+    }
+
+    protected override async ValueTask VisitEnumValuesDefinitionAsync(GraphQLEnumValuesDefinition enumValuesDefinition, WriterContext context)
+    {
+        await VisitAsync(enumValuesDefinition.Comments, context).ConfigureAwait(false);
+
+        for (var i = 0; i < enumValuesDefinition.Count; i++)
+        {
+            var isLastItem = i + 1 >= enumValuesDefinition.Count;
+            var enumValueDefinition = enumValuesDefinition.Items[i];
+
+            await VisitAsync(enumValueDefinition.Comments, context).ConfigureAwait(false);
+            await VisitAsync(enumValueDefinition.Description, context).ConfigureAwait(false);
+            await VisitAsync(enumValueDefinition.Directives, context).ConfigureAwait(false);
+
+            await WriteJsonPropertyAttributeAsync(enumValueDefinition.EnumValue.Name, context);
+            await context.WriteLineAsync(enumValueDefinition.EnumValue.Name.StringValue + (isLastItem ? "" : ","));
+
+            if (!isLastItem)
+                await context.WriteEmptyLineAsync();
+        }
+    }
+
+    protected override async ValueTask VisitObjectTypeDefinitionAsync(GraphQLObjectTypeDefinition objectTypeDefinition, WriterContext context)
+    {
+        await VisitAsync(objectTypeDefinition.Comments, context).ConfigureAwait(false);
+        await VisitAsync(objectTypeDefinition.Description, context).ConfigureAwait(false);
+
+        await context.WriteLineAsync("public record " + MakePascalCase(objectTypeDefinition.Name));
+        await VisitAsync(objectTypeDefinition.Interfaces, context).ConfigureAwait(false);
+
+        await context.WriteLineAsync("{");
+        context.Indent();
+
+        if (objectTypeDefinition.Directives is not null)
+            await VisitAsync(objectTypeDefinition.Directives, context).ConfigureAwait(false);
+
+        if (objectTypeDefinition.Fields is not null)
+            await VisitAsync(objectTypeDefinition.Fields, context).ConfigureAwait(false);
+
+        context.Outdent();
+        await context.WriteLineAsync("}");
+    }
+
+    protected override async ValueTask VisitFieldsDefinitionAsync(GraphQLFieldsDefinition fieldsDefinition, WriterContext context)
+    {
+        await VisitAsync(fieldsDefinition.Comments, context).ConfigureAwait(false);
+
+        // Visitor may change AST while being traversed so foreach yields
+        // System.InvalidOperationException: 'Collection was modified; enumeration operation may not execute.'
+        for (var i = 0; i < fieldsDefinition.Items.Count; ++i)
+        {
+            var isLastItem = i + 1 >= fieldsDefinition.Items.Count;
+
+            await VisitAsync(fieldsDefinition.Items[i], context).ConfigureAwait(false);
+
+            if (!isLastItem)
+                await context.WriteEmptyLineAsync();
+        }
+    }
+
+    protected override async ValueTask VisitFieldDefinitionAsync(GraphQLFieldDefinition fieldDefinition, WriterContext context)
+    {
+        var fieldType = MapGraphTypeToTypeName(fieldDefinition.Type);
+
+        await VisitAsync(fieldDefinition.Comments, context).ConfigureAwait(false);
+        await VisitAsync(fieldDefinition.Description, context).ConfigureAwait(false);
+
+        await WriteJsonPropertyAttributeAsync(fieldDefinition.Name, context);
+        await context.WriteLineAsync($$"""public {{fieldType}} {{MakePascalCase(fieldDefinition.Name)}} { get; set; }""");
+
+        await VisitAsync(fieldDefinition.Arguments, context).ConfigureAwait(false);
+        await VisitAsync(fieldDefinition.Directives, context).ConfigureAwait(false);
+    }
+
+    protected override async ValueTask VisitInterfaceTypeDefinitionAsync(GraphQLInterfaceTypeDefinition interfaceTypeDefinition, WriterContext context)
+    {
+        await VisitAsync(interfaceTypeDefinition.Comments, context).ConfigureAwait(false);
+        await VisitAsync(interfaceTypeDefinition.Description, context).ConfigureAwait(false);
+
+        await context.WriteLineAsync("public interface I" + MakePascalCase(interfaceTypeDefinition.Name));
+        await VisitAsync(interfaceTypeDefinition.Interfaces, context).ConfigureAwait(false);
+
+        await context.WriteLineAsync("{");
+        context.Indent();
+
+        if (interfaceTypeDefinition.Directives is not null)
+            await VisitAsync(interfaceTypeDefinition.Directives, context).ConfigureAwait(false);
+
+        if (interfaceTypeDefinition.Fields is not null)
+            await VisitAsync(interfaceTypeDefinition.Fields, context).ConfigureAwait(false);
 
         context.Outdent();
         await context.WriteLineAsync("}");
