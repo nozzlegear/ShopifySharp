41f6e17160baa19bbab2007df886c91c8bf609f6
Joshua Harms <joshua@nozzlegear.com>
2025-06-25 20:51:08 -0500

    Implement functional GraphQL parser/file writer using pipeWriter computation builder

---
+++ ShopifySharp.GraphQL.ParserF/PipeWriterComputationBuilder.fs
@@ -0,0 +1,83 @@
+namespace ShopifySharp.GraphQL.ParserF
+
+open System
+open System.IO.Pipelines
+open System.Text
+open System.Threading.Tasks
+
+type PipeWriterComputationBuilder(writer: PipeWriter) =
+    member _.Bind(task': ValueTask, continuation: unit -> ValueTask) : ValueTask =
+        ValueTask(task {
+            do! task'.ConfigureAwait(false)
+            do! continuation ()
+        })
+
+    member _.Bind(value: ReadOnlyMemory<byte>, continuation: unit -> ValueTask) : ValueTask =
+        ValueTask(task {
+            let! _ = writer.WriteAsync(value)
+            do! continuation ()
+        })
+
+    member _.Bind(value: string, continuation: unit -> ValueTask) : ValueTask =
+        ValueTask(task {
+            let bytes = Encoding.UTF8.GetBytes(value)
+            let! _ = writer.WriteAsync(bytes)
+            do! continuation ()
+        })
+
+    member _.Bind(value: string, continuation: FlushResult -> ValueTask) : ValueTask =
+        ValueTask(task {
+            let bytes = Encoding.UTF8.GetBytes(value)
+            let! flushResult = writer.WriteAsync(bytes)
+            do! continuation flushResult
+        })
+
+    member _.Return(_: unit) : ValueTask =
+        ValueTask.CompletedTask
+
+    member _.ReturnFrom(task: ValueTask) : ValueTask =
+        task
+
+    member _.Zero() : ValueTask =
+        ValueTask.CompletedTask
+
+    member _.Delay(f: unit -> ValueTask) : unit -> ValueTask =
+        f
+
+    /// The forLoop operator.
+    member this.For(collection: seq<_>, func: _ -> ValueTask) : ValueTask =
+        ValueTask(task {
+            let ie = collection.GetEnumerator()
+            try
+                while ie.MoveNext() do
+                    let value = ie.Current
+                    do! func value
+            finally
+                ie.Dispose()
+        })
+
+    member _.Yield _ = ValueTask.CompletedTask
+
+    member _.Yield(value: string) : ValueTask =
+        ValueTask(task {
+            let bytes = Encoding.UTF8.GetBytes(value)
+            let! _ = writer.WriteAsync(bytes)
+            ()
+        })
+
+    member _.YieldFrom(continuation: PipeWriter -> ValueTask) : ValueTask =
+        ValueTask(task {
+            do! continuation writer
+        })
+
+    member _.Combine(first: ValueTask, second: unit -> ValueTask) : ValueTask =
+        ValueTask(task {
+            do! first
+            do! second ()
+        })
+
+    member _.Run(computation: unit -> ValueTask) : ValueTask =
+        computation()
+
+module PipeWriter =
+    let pipeWriter (writer: PipeWriter) = PipeWriterComputationBuilder(writer)
---
+++ ShopifySharp.GraphQL.ParserF/Writer.fs
@@ -0,0 +1,328 @@
+module ShopifySharp.GraphQL.ParserF.Writer
+
+open System
+open System.IO
+open System.IO.Pipelines
+open System.Linq
+open System.Text
+open System.Threading
+open System.Threading.Tasks
+open Microsoft.CodeAnalysis;
+open Microsoft.CodeAnalysis.CSharp;
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open ShopifySharp.GraphQL.ParserF.PipeWriter;
+
+type private Writer = PipeWriter
+
+let private (~%) job = ignore job
+
+let private NewLine = Environment.NewLine
+
+let private toTab = function
+    | Indented -> "\t"
+    | Outdented -> String.Empty
+
+let private writeFileToPath filePath (fileText: string) cancellationToken: ValueTask =
+    ValueTask(task {
+        if File.Exists(filePath) then
+            File.Delete(filePath)
+
+        let directory = Path.GetDirectoryName filePath
+
+        if directory <> "" && not (Directory.Exists directory) then
+            %Directory.CreateDirectory(directory)
+
+        do! File.WriteAllTextAsync(filePath, fileText, cancellationToken)
+    })
+
+let private parseCsharpStringToGeneratedFiles (csharpCode: string) cancellationToken: ValueTask<GeneratedCsharpFile[]> =
+    ValueTask<GeneratedCsharpFile[]>(task {
+        let! csharpTree = CSharpSyntaxTree.ParseText(csharpCode).GetRootAsync(cancellationToken)
+        let syntaxRoot = csharpTree :?> CompilationUnitSyntax;
+        let usings = syntaxRoot.Usings;
+        let externals = syntaxRoot.Externs
+
+        return
+            syntaxRoot.Members.OfType<BaseNamespaceDeclarationSyntax>()
+            |> Seq.collect (fun ns -> ns.Members.OfType<BaseTypeDeclarationSyntax>()
+                                      |> Seq.map (fun types -> (ns, types)))
+            |> Seq.map (fun (ns, type') ->
+                let unit = SyntaxFactory.CompilationUnit()
+                            .WithExterns(externals)
+                            .WithUsings(usings)
+                            .AddMembers(ns.WithMembers(SyntaxFactory.SingletonList<MemberDeclarationSyntax>(type')))
+                            .NormalizeWhitespace(eol = Environment.NewLine)
+
+                { FileName = type'.Identifier.Text + ".cs"
+                  FileText = unit.ToFullString() }
+            )
+            |> Array.ofSeq
+    })
+
+let private readPipe (reader: PipeReader) cancellationToken: ValueTask<StringBuilder> =
+    let sb = StringBuilder()
+    let rec loop () = task {
+        let! result = reader.ReadAsync(cancellationToken).ConfigureAwait(false)
+
+        let mutable enumerator = result.Buffer.GetEnumerator()
+        while enumerator.MoveNext() do
+            %sb.Append(Encoding.UTF8.GetString(enumerator.Current.Span))
+
+        reader.AdvanceTo(result.Buffer.End)
+
+        if not (result.IsCompleted || result.IsCanceled) then
+            do! loop()
+    }
+
+    ValueTask<StringBuilder>(task {
+        do! loop()
+        do! reader.CompleteAsync().ConfigureAwait(false)
+        return sb
+    })
+
+let private sanitizeString (str: string): string =
+    str.ReplaceLineEndings("")
+       .Replace("\"", "", StringComparison.OrdinalIgnoreCase)
+       .Replace("'", "", StringComparison.OrdinalIgnoreCase)
+
+let private toCasing casing (str: string): string =
+    let first = str[0]
+    let rest  = str[1..]
+    match casing with
+    | Pascal -> Char.ToUpper(first).ToString() + rest
+    | Camel -> Char.ToLower(first).ToString() + rest
+
+let private mapValueTypeToString = function
+    | FieldValueType.ULong -> "ulong"
+    | FieldValueType.Long -> "long"
+    | FieldValueType.Int -> "int"
+    | FieldValueType.Decimal -> "decimal"
+    | FieldValueType.Float -> "float"
+    | FieldValueType.Boolean -> "bool"
+    | FieldValueType.String -> "string"
+    | FieldValueType.DateTime -> "DateTime"
+    | FieldValueType.DateOnly -> "DateOnly"
+    | FieldValueType.TimeSpan -> "TimeSpan"
+    | FieldValueType.GraphObjectType graphObjectTypeName -> graphObjectTypeName
+
+let rec private mapFieldTypeToString (valueType: FieldType) =
+    match valueType with
+    | NullableType nullableType -> $"{mapFieldTypeToString nullableType}?"
+    | NonNullableType nonNullableType -> mapFieldTypeToString nonNullableType
+    | CollectionType collectionType ->  $"ICollection<{mapFieldTypeToString collectionType}>"
+    | ValueType valueType -> mapValueTypeToString valueType
+
+let private writeNamespaceAndUsings (writer: Writer) : ValueTask =
+    pipeWriter writer {
+        do! "namespace ShopifySharp.GraphQL;"
+        do! NewLine
+        do! "using System;"
+        do! NewLine
+        do! "using System.Text.Json;"
+        do! NewLine
+    }
+
+let private writeSummary  indentation (summary: string[]) writer : ValueTask =
+    let indentation = toTab indentation
+    pipeWriter writer {
+        for line in summary do
+            do! indentation
+            do! line
+            do! NewLine
+    }
+
+let private writeDeprecationAttribute indentation (deprecationWarning: string option) writer : ValueTask =
+    let indentation = toTab indentation
+    pipeWriter writer {
+        match deprecationWarning with
+        | Some x when String.IsNullOrWhiteSpace x ->
+            do! indentation
+            do! "[Obsolete]"
+            do! NewLine
+        | Some x ->
+            do! indentation
+            do! $"[Obsolete(\"{sanitizeString x}\")]"
+            do! NewLine
+        | None ->
+            ()
+    }
+
+let private writeJsonPropertyAttribute (propertyName: string) writer : ValueTask =
+    pipeWriter writer {
+        do! "\t" + $"[JsonProperty(\"{propertyName}\")]"
+        do! NewLine
+    }
+
+let private writeJsonDerivedTypeAttributes (typeNames: string[]) writer: ValueTask =
+    pipeWriter writer {
+        do! "[(JsonPolymorphic(TypeDiscriminatorPropertyName = \"__typename\")]"
+        do! NewLine
+
+        for typeName in typeNames do
+            do! $"""[(JsonDerivedType(typeof({typeName}), typeDiscriminator: "{typeName}")]"""
+            do! NewLine
+    }
+
+let private writeFields (fields: Field[]) writer : ValueTask =
+    pipeWriter writer {
+        for field in fields do
+            let fieldType = mapFieldTypeToString field.ValueType
+
+            yield! writeSummary Indented field.XmlSummary
+            yield! writeJsonPropertyAttribute field.Name
+            yield! writeDeprecationAttribute Indented field.Deprecation
+
+            do! (toTab Indented) + $$"""public {{fieldType}} {{toCasing Casing.Camel field.Name}} { get; set; }"""
+            do! NewLine
+    }
+
+let private writeClass (class': Class) (writer: Writer): ValueTask =
+    pipeWriter writer {
+        yield! writeSummary Outdented class'.XmlSummary
+        yield! writeDeprecationAttribute Outdented class'.Deprecation
+
+        do! $"public record {class'.Name}: ShopifyGraphObject"
+
+        if class'.InheritedTypeNames.Length > 0 then
+            do! ", "
+            do! String.Join(", ", class'.InheritedTypeNames)
+
+        do! NewLine
+        do! "{"
+        do! NewLine
+
+        yield! writeFields class'.Fields
+
+        do! "}"
+        do! NewLine
+    }
+
+let private writeInterface (interface': Interface) (writer: Writer): ValueTask =
+    pipeWriter writer {
+        yield! writeSummary Outdented interface'.XmlSummary
+        yield! writeDeprecationAttribute Outdented interface'.Deprecation
+
+        do! $"public interface {interface'.Name}"
+
+        if interface'.InheritedTypeNames.Length > 0 then
+            do! ": "
+            do! String.Join(", ", interface'.InheritedTypeNames)
+
+        do! NewLine
+        do! "{"
+        do! NewLine
+
+        yield! writeFields interface'.Fields
+
+        do! "}"
+        do! NewLine
+    }
+
+let private writeEnum (enum: VisitedEnum) (writer: Writer): ValueTask =
+    pipeWriter writer {
+        yield! writeSummary Outdented enum.XmlSummary
+        yield! writeDeprecationAttribute Outdented enum.Deprecation
+
+        do! $"public enum {enum.Name}"
+        do! NewLine
+        do! "{"
+        do! NewLine
+
+        for case in enum.Cases do
+            yield! writeDeprecationAttribute Outdented case.Deprecation
+
+            do! $"\t{case.Name}"
+            if Option.isSome case.Value then
+                do! " = " + (Option.get case.Value)
+            do! ","
+            do! NewLine
+
+        do! "}"
+        do! NewLine
+    }
+
+let private writeInputObject (inputObject: InputObject) (writer: Writer): ValueTask =
+    pipeWriter writer {
+        yield! writeSummary Outdented inputObject.XmlSummary
+        yield! writeDeprecationAttribute Outdented inputObject.Deprecation
+
+        do! $"public record {inputObject.Name}: ShopifyGraphInputObject"
+
+        do! NewLine
+        do! "{"
+        do! NewLine
+
+        yield! writeFields inputObject.Fields
+
+        do! "}"
+        do! NewLine
+    }
+
+let private writeUnionType (unionType: UnionType) (writer: Writer): ValueTask =
+    pipeWriter writer {
+        yield! writeSummary Outdented unionType.XmlSummary
+        yield! writeDeprecationAttribute Outdented unionType.Deprecation
+        yield! writeJsonDerivedTypeAttributes unionType.Types
+
+        do! $"public interface I{unionType.Name}Union: IShopifyGraphUnionType<"
+        do! String.Join(", ", unionType.Types)
+        do! ">"
+
+        do! NewLine
+        do! "{"
+        do! NewLine
+
+        // These default interface methods are only usable in .NET 6.0 and above - anything lower will cause
+        // the compiler to throw an error.
+        do! "#IF NET6_0_OR_GREATER"
+        do! NewLine
+
+        for typeName in unionType.Types do
+            do! (toTab Indented) + $"public {typeName}? As{typeName}() => this as {typeName};"
+            do! NewLine
+
+        do! "#ENDIF"
+        do! NewLine
+        do! "}"
+        do! NewLine
+    }
+
+let writeVisitedTypesToPipe (writer: Writer) (visitedTypes: VisitedTypes[]) (_: CancellationToken): ValueTask =
+    pipeWriter writer {
+        // Always write the namespace and usings at the very top of the document
+        yield! writeNamespaceAndUsings
+
+        for visitedType in visitedTypes do
+            match visitedType with
+            | Class class' ->
+                yield! writeClass class'
+            | Interface interface' ->
+                yield! writeInterface interface'
+            | Enum enum' ->
+                yield! writeEnum enum'
+            | InputObject inputObject ->
+                yield! writeInputObject inputObject
+            | UnionType unionType ->
+                yield! writeUnionType unionType
+    }
+
+let writeVisitedTypesToFileSystem (outDir: string) (visitedTypes: VisitedTypes[]) cancellationToken: ValueTask =
+    ValueTask(task {
+        let pipe = Pipe(PipeOptions(pauseWriterThreshold = 0, resumeWriterThreshold = 0))
+        let readTask = (readPipe pipe.Reader cancellationToken).ConfigureAwait(false)
+
+        do! writeVisitedTypesToPipe pipe.Writer visitedTypes cancellationToken
+        do! pipe.Writer.CompleteAsync().ConfigureAwait(false);
+
+        let! csharpCode = readTask
+
+        // Write all generated code to a temporary file
+        do! writeFileToPath "temp.cs" (csharpCode.ToString()) cancellationToken
+
+        // Parse the generated code into individual files
+        let! generatedFiles = parseCsharpStringToGeneratedFiles (csharpCode.ToString()) cancellationToken
+
+        for file in generatedFiles do
+            let filePath = Path.Join(outDir, "/", file.FileName)
+            do! writeFileToPath filePath file.FileText cancellationToken
+    })
