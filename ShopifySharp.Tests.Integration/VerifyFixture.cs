c0c89d3bcb9fa9d94a3c1282ab7d7b56f4aa136f
Joshua Harms <joshua@nozzlegear.com>
2025-08-26 02:19:49 -0500

    Add integration tests for bug described in #1205
    type: fix
    scope: graphql, tests

--- ShopifySharp.Tests.Integration/VerifyFixture.cs
+++ ShopifySharp.Tests.Integration/VerifyFixture.cs
@@ -9,6 +9,8 @@ public class VerifyFixture
         Settings.ScrubMembers(info => info.Name is "LegacyResourceId" or "legacyResourceId");
         Settings.ScrubMembers(info => info.Name is "Id" or "id");
         Settings.ScrubMembers(info => info.Name is "RequestId" or "requestId");
+        Settings.ScrubMembers(info => info.Name is "ShippingAddress" or "shippingAddress");
+        Settings.ScrubMembers(info => info.Name is "BillingAddress" or "billingAddress");
         Settings.ScrubMembers(info => info.DeclaringType?.Name is "PageInfo" or "pageInfo");
         Settings.UseDirectory("Snapshots");
     }
