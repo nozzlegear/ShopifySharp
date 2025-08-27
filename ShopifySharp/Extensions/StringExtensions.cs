6d3024403be826eac4d9457625d2282793d46f5c
Joshua Harms <joshua@nozzlegear.com>
2025-08-27 18:55:03 -0500

    Use StringComparison.OrdinalIgnoreCase to compare strings
    type: refactor
    scope: utils

--- ShopifySharp/Extensions/StringExtensions.cs
+++ ShopifySharp/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ShopifySharp;
 
 internal static class StringExtensions
@@ -5,15 +7,15 @@ internal static class StringExtensions
     /// <summary>
     /// Checks if a string starts with another string, ignoring case.
     /// </summary>
-    public static bool StartsWithIgnoreCase(this string str, string a) => str.ToLower().StartsWith(a.ToLower());
+    public static bool StartsWithIgnoreCase(this string str, string a) => str.StartsWith(a, StringComparison.OrdinalIgnoreCase);
 
     /// <summary>
     /// Checks if a string ends with another string, ignoring case.
     /// </summary>
-    public static bool EndsWithIgnoreCase(this string str, string a) => str.ToLower().EndsWith(a.ToLower());
+    public static bool EndsWithIgnoreCase(this string str, string a) => str.EndsWith(a, StringComparison.OrdinalIgnoreCase);
 
     /// <summary>
-    /// Checks if a string contains another string, ignorning case.
+    /// Checks if a string contains another string, ignoring case.
     /// </summary>
-    public static bool ContainsIgnoreCase(this string str, string a) => str.ToLower().Contains(a.ToLower());
+    public static bool ContainsIgnoreCase(this string str, string a) => str.IndexOf(a, StringComparison.OrdinalIgnoreCase) >= 0;
 }
