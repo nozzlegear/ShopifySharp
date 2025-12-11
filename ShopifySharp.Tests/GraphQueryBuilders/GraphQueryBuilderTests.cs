d3bbd3d6c9405dd0fe6a894c1103e413ce041955
Joshua Harms <joshua@nozzlegear.com>
2025-12-11 17:25:06 -0600

    fixup! fixup! Add tests for GraphQueryBuilder functionality

--- ShopifySharp.Tests/GraphQueryBuilders/GraphQueryBuilderTests.cs
+++ ShopifySharp.Tests/GraphQueryBuilders/GraphQueryBuilderTests.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using ShopifySharp.GraphQL;
 using ShopifySharp.Infrastructure;
 using ShopifySharp.Services.Generated;
 using ShopifySharp.Tests.Fixtures;
@@ -181,4 +182,65 @@ public class GraphQueryBuilderTests(VerifyFixture verifyFixture): IClassFixture<
     }
 
     #endregion
+
+    #region Unions
+
+    [Fact]
+    public async Task QueryBuilder_AddUnionCase_ShouldBuild()
+    {
+        // Setup
+        var sut = new CalculatedDiscountCodeApplicationQueryBuilder();
+        sut.AddFieldCode();
+        sut.AddUnionCaseValue((MoneyV2QueryBuilder x) => x.AddFieldAmount());
+
+        // Act
+        var result = sut.Build();
+
+        // Assert
+        await Verify(result, _verifySettings);
+    }
+
+    [Fact]
+    public async Task QueryBuilder_AddUnionCase_WhenAddingMoreThanOneUnion_ShouldBuild()
+    {
+        // Setup
+        var sut = new CalculatedDiscountCodeApplicationQueryBuilder();
+        sut.AddFieldCode();
+        sut.AddUnionCaseValue((MoneyV2QueryBuilder x) => x.AddFieldAmount());
+
+        // Act
+        var result = sut.Build();
+
+        // Assert
+        await Verify(result, _verifySettings);
+    }
+
+    [Fact]
+    public async Task QueryBuilder_AddUnionCase_WhenAddingCustomUnionTypes_ShouldBuild()
+    {
+        // Setup
+        var sut = new CalculatedDiscountCodeApplicationQueryBuilder();
+        sut.AddFieldCode();
+        sut.AddUnionCaseValue((MoneyV2QueryBuilder x) => x.AddFieldAmount());
+        sut.AddUnionCase<CustomThing, CustomThingQueryBuilder>("myCustomThing", x => x.AddFoo());
+
+        // Act
+        var result = sut.Build();
+
+        // Assert
+        await Verify(result, _verifySettings);
+    }
+
+    #endregion
+
+    public class CustomThingQueryBuilder() : GraphQueryBuilder<CustomThing>("customThing")
+    {
+        public CustomThingQueryBuilder AddFoo()
+        {
+            AddField("foo");
+            return this;
+        }
+    }
+
+    public class CustomThing : IGraphQLUnionCase, IGraphQLObject;
 }
