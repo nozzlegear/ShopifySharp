42ee86e0adb48e3c27f5d1b32c14d2c02c91c6e6
Joshua Harms <joshua@nozzlegear.com>
2025-12-13 19:19:38 -0600

    fixup! Add `__typename` field to GraphQL UnionCase's select list

--- ShopifySharp/Infrastructure/Query.cs
+++ ShopifySharp/Infrastructure/Query.cs
@@ -126,7 +126,6 @@ public class Query<TSource> : IQuery<TSource>
     {
         RequiredArgument.NotNull(union, nameof(union));
 
-        SelectList.Add(union);
         // Ensure we also select the __typename, which is required for deserializing union cases
         SelectList.Add("__typename");
 
