8dd40abe816270523a079b37d66d7151303eaede
Joshua Harms <joshua@nozzlegear.com>
2025-12-20 15:10:05 -0600

    WIP: Add experimental OrderQueryFieldsBuilder implementation
    type: wip
    scope: graphquerybuilder

--- ShopifySharp/Infrastructure/GraphQueryBuilder.cs
+++ ShopifySharp/Infrastructure/GraphQueryBuilder.cs
@@ -25,8 +25,42 @@ public abstract class GraphQueryBuilder<T>
     public string Build() => Query.Build();
 }
 
+public sealed class OrderQueryArgumentsBuilder(IQuery<ShopifySharp.GraphQL.Order> query)
+{
+    public OrderQueryArgumentsBuilder Id(string id)
     {
         return this;
     }
+}
+
+public sealed class OrderQueryFieldsBuilder(IQuery<ShopifySharp.GraphQL.Order> query)
+{
+    public OrderQueryFieldsBuilder Name() => this;
+
+    public OrderQueryFieldsBuilder DateCreated() => this;
+
+    public OrderQueryFieldsBuilder DateUpdated() => this;
+
+    public OrderQueryFieldsBuilder Id() => this;
+}
+
+public sealed class OrderQueryUnionsBuilder(IQuery<ShopifySharp.GraphQL.Order> query)
+{
+    public OrderQueryUnionsBuilder PurchasingEntityCustomer(Action<PurchasingEntity> build) => this;
+    public OrderQueryUnionsBuilder PurchasingEntityCompany(Action<PurchasingEntity> build) => this;
+}
+
+public sealed class OrderQueryBuilder : GraphQueryBuilder<ShopifySharp.GraphQL.Order>
+{
+    public OrderQueryBuilder(QueryOptions? queryOptions = null) : base("order", queryOptions)
+    {
+        Arguments = new OrderQueryArgumentsBuilder(Query);
+        Fields = new OrderQueryFieldsBuilder(Query);
+        Unions = new OrderQueryUnionsBuilder(Query);
+    }
+
+    public OrderQueryArgumentsBuilder Arguments { get; }
+    public OrderQueryFieldsBuilder Fields { get; }
+    public OrderQueryUnionsBuilder Unions { get; }
 }
 
