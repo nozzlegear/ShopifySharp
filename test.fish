4084a37387dbea54eb0cf3e5084a2a04f093c9e2
Joshua Harms <joshua@nozzlegear.com>
2026-01-07 00:17:55 -0600

    Exit early if regenerate.fish fails
    type: refactor
    scope: dev

--- test.fish
+++ test.fish
@@ -4,5 +4,6 @@ git clean -df ShopifySharp/Entities/GraphQL/Generated
 and git checkout ShopifySharp/Entities/GraphQL/Generated
 rm -r ShopifySharp/Entities/GraphQL/Generated
 ./regenerate.fish
+or return 1
 dotnet build ShopifySharp
 
