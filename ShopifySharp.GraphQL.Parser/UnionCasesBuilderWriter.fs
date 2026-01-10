2f36f2dbb561fa3a9cfca101459ab841c91418c4
Joshua Harms <joshua@nozzlegear.com>
2026-01-10 02:15:01 -0600

    Rename UnionFieldsBuilderWriter to UnionCasesBuilderWriter
    type: refactor
    scope: graphql-parser

--- ShopifySharp.GraphQL.Parser/UnionCasesBuilderWriter.fs
+++ ShopifySharp.GraphQL.Parser/UnionCasesBuilderWriter.fs
@@ -10,8 +10,11 @@ type UnionCasesBuilderWriter(
     unionCaseNames: string array,
     deprecation: string option,
     context: IParsedContext
-) =
-    let builderClassName = toBuilderName (UnionCasesBuilder fieldName)
+) as self =
+    let builderClassName = toBuilderName (UnionCasesBuilder parentType.Name)
+    let genericTypeName =
+        toGenericType parentType context.AssumeNullability
+        |> qualifiedPascalTypeName
 
     let writeOnUnionCaseMethod (unionCaseName: string) writer: ValueTask =
         let pascalUnionCaseName = toCasing Pascal unionCaseName
@@ -22,11 +25,11 @@ type UnionCasesBuilderWriter(
             do! NewLine
             do! Indented + "{"
             do! NewLine
-            do! DoubleIndented + $"var queryBuilder = new {unionCaseQueryBuilderName}(\"... on {pascalUnionCaseName}\");"
+            do! DoubleIndented + $$"""var queryBuilder = new {{unionCaseQueryBuilderName}}("... on {{pascalUnionCaseName}}");"""
             do! NewLine
-            do! DoubleIndented + "build(queryBuilder);"
+            do! DoubleIndented + "build.Invoke(queryBuilder);"
             do! NewLine
-            do! DoubleIndented + $"_query.AddField<{pascalUnionCaseName}>(queryBuilder.Query);"
+            do! DoubleIndented + $$"""base.Query.AddField(queryBuilder.Query);"""
             do! NewLine
             do! DoubleIndented + "return this;"
             do! NewLine
@@ -37,32 +40,27 @@ type UnionCasesBuilderWriter(
     member _.BuilderClassName = builderClassName
     member _.CamelFieldName = toCasing Camel fieldName
     member _.PascalFieldName = toCasing Pascal fieldName
+    member _.GenericTypeName = genericTypeName
     member _.DeprecationWarning = deprecation
 
     member _.WriteToPipewriter writer: ValueTask =
         pipeWriter writer {
-            do! $"public sealed class {builderClassName}"
+            do! $"public sealed class {builderClassName} : UnionCaseBuilderBase<{genericTypeName}>"
             do! NewLine
             do! "{"
             do! NewLine
-            do! Indented + "private readonly IQuery<object> _query;"
+            do! Indented + $$"""public {{builderClassName}}(string fieldName = "{{self.CamelFieldName}}") : this(new Query<{{genericTypeName}}>(fieldName))"""
+            do! NewLine
+            do! Indented + "{}"
             do! NewLine + NewLine
-            do! Indented + $"public {builderClassName}(string fieldName)"
+            do! Indented + $$"""public {{builderClassName}}(IQuery<{{genericTypeName}}> query) : base(query)"""
             do! NewLine
-            do! Indented + "{"
-            do! NewLine
-            do! DoubleIndented + "_query = new Query<object>(fieldName);"
-            do! NewLine
-            do! DoubleIndented + "_query.AddField(\"__typename\");"
-            do! NewLine
-            do! Indented + "}"
+            do! Indented + "{}"
             do! NewLine + NewLine
 
             for unionCaseName in unionCaseNames do
                 yield! writeOnUnionCaseMethod unionCaseName
                 do! NewLine
 
-            do! Indented + "public IQuery<object> Query => _query;"
-            do! NewLine
             do! "}"
         }
