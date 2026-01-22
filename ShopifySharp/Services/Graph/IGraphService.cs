0a5d79ea6b219ec18d7e2653d5b0d6632bea2d5f
Joshua Harms <joshua@nozzlegear.com>
2026-01-22 16:48:43 -0600

    Add support for passing query builders to the GraphService
    This lets us construct an entire GraphQL query using the new fluent
    query builders, then pass them directly to the GraphService to be
    serialized into an http request. We can even infer the return type of
    the request automatically using the generic type of the query builder!
    
    type: feature
    scope: graphservice, graphql

--- ShopifySharp/Services/Graph/IGraphService.cs
+++ ShopifySharp/Services/Graph/IGraphService.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using ShopifySharp.GraphQL;
 using ShopifySharp.Services.Graph;
 
 namespace ShopifySharp;
@@ -58,4 +59,22 @@ public interface IGraphService : IShopifyService
     /// <paramref name="resultType"/>.
     /// </returns>
     Task<GraphResult<object>> PostAsync(GraphRequest graphRequest, Type resultType, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Executes the GraphQL API call using the query and variables described in <paramref name="graphRequest" />. This
+    /// query automatically scopes the response from Shopify to the <c>data</c> property and deserializes it to type
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="graphRequest">
+    /// An object containing the GraphQL query to be executed. Variables are optional.
+    /// Use <see cref="GraphRequest.EstimatedQueryCost" /> to provide an estimated query cost to any
+    /// <see cref="IRequestExecutionPolicy" />, to avoid sending a request that will be throttled.
+    /// </param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>
+    /// A <see cref="GraphResult{T}"/> of type <typeparamref name="T"/>. The <see cref="GraphResult{T}.Data"/> value
+    /// will be scoped to the <c>data</c> property of the graph response.
+    /// </returns>
+    Task<GraphResult<T>> PostAsync<T>(GraphRequest<T> graphRequest, CancellationToken cancellationToken = default)
+        where T : IGraphQLObject;
 }
