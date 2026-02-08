b65fe25f71f4124cf4ed3f39b4e5444fd4d1deef
Joshua Harms <joshua@nozzlegear.com>
2026-02-08 14:19:54 -0600

    Make ProductOptionValue.HasVariants nullable
    This prevents unnecessary serialization in the Verified test results
    file.
    
    type: refactor
    scope: tests

--- ShopifySharp.Tests.Integration/Features/Products/Models/ProductOptionValue.cs
+++ ShopifySharp.Tests.Integration/Features/Products/Models/ProductOptionValue.cs
@@ -3,5 +3,5 @@ namespace ShopifySharp.Tests.Integration.Features.Products.Models;
 public record ProductOptionValue(
     string Id,
     string Name,
-    bool HasVariants
+    bool? HasVariants
 );
