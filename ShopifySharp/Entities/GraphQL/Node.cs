87a70cf4212fcbcf61e5260aa23bbb1d4b9b882f
Joshua Harms <joshua@nozzlegear.com>
2026-07-11 15:56:04 -0500

    Allow GraphQL parser to generate INode interface
    This commit removes the hardcoded INode interface and allows the parser
    to generate it instead, which adds automatic support for deserializing
    any object that implements the INode interface using json polymorphism –
    something that wasn't possible before without also hardcoding 180+
    [JsonPolymorphic] attributes.
    
    type: enhancement
    scope: graphql-parser

--- ShopifySharp/Entities/GraphQL/Node.cs
+++ ShopifySharp/Entities/GraphQL/Node.cs
@@ -3,14 +3,8 @@ using System.Text.Json.Serialization;
 
 namespace ShopifySharp.GraphQL;
 
-public interface INode : IGraphQLObject
-{
-    // ReSharper disable once InconsistentNaming
-    string? id { get; }
-}
-
 public sealed record Node : INode
 {
     [JsonPropertyName("id")]
-    public string? id { get; } = null;
+    public string? id { get; set; } = null;
 }
